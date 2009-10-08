17977bb9db2bb8560daba330099f217793f8ce47
voyteckst <voyteckst@piwigo.org>
2009-10-08 19:34:26 +0000

    git-svn-id: http://piwigo.org/svn/extensions/community@3992 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+//Polish transaltion voyteckst
+
+$lang['Community'] = 'Społeczność';
+$lang['Add permissions'] = 'Dodaj uprawnienia';
+$lang['Permission level'] = 'Poziom uprawnień';
+$lang['Community level 1'] = 'dodaj zdjęcia';
+$lang['Community level 2'] = 'dodaj zdjęcia + kategorie';
+$lang['community permissions "%s" added/updated for "%s"'] = 'dodano/zautualizowano uprawnienia społecznościowe "%s" dla "%s"';
+$lang['community permissions "%s" removed for "%s"'] = 'Usunięto uprawnienia społecznościowe "%s" dla "%s"';
+?>
