19035a0596c63365dcf9c7f2ff71e9590c9ca5dc
keeshessels <keeshessels@piwigo.org>
2010-04-13 14:22:04 +0000

    Nl-NL added
    git-svn-id: http://piwigo.org/svn/extensions/community@5831 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Community'] = 'Community';
+$lang['Add permissions'] = 'Machtigingen toevoegen';
+$lang['Permission level'] = 'Machtigingen-niveau';
+$lang['Community level 1'] = 'Afbeeldingen toevoegen';
+$lang['Community level 2'] = 'Afbeeldingen + categorieën toevoegen';
+$lang['community permissions "%s" added/updated for "%s"'] = 'Community machtigingen "%s" toegevoegd/bijgewerkt van "%s"';
+$lang['community permissions "%s" removed for "%s"'] = 'Community machtigingen "%s" verwijderd van "%s"';
+?>
