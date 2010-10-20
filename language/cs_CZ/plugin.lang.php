edb4919c621c7dac566ded4a5bfe45a24f1642f9
ddtddt <ddtddt@piwigo.org>
2010-10-20 14:41:03 +0000

    [extension] community add cs_CZ thanks to webprostor
    git-svn-id: http://piwigo.org/svn/extensions/community@7306 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Community'] = 'Komunita';
+$lang['Add permissions'] = 'Přidat oprávnění';
+$lang['Permission level'] = 'Stupeň oprávnění';
+$lang['Community level 1'] = 'vložení fotek';
+$lang['Community level 2'] = 'vložení fotek + kategorií';
+$lang['community permissions "%s" added/updated for "%s"'] = 'oprávnění komunity "%s" přidaní/aktualizace pro "%s"';
+$lang['community permissions "%s" removed for "%s"'] = 'oprávnění komunity "%s" odstranění pro "%s"';
+?>
