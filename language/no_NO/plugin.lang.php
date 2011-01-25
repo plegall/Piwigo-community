215a9a05c579acab6eb4470ed544f10e8cc80451
ddtddt <ddtddt@piwigo.org>
2011-01-25 13:42:05 +0000

    [extensions] - community - add no_NO (Norwegian) thanks to atlew - Step 1
    git-svn-id: http://piwigo.org/svn/extensions/community@8879 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/no_NO/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Community'] = 'Fellesskap';
+$lang['Add permissions'] = 'Legg til rettigheter';
+$lang['Permission level'] = 'Rettighets nivå';
+$lang['Community level 1'] = 'legg til bilder';
+$lang['Community level 2'] = 'legg til bilder + album';
+$lang['community permissions "%s" added/updated for "%s"'] = 'fellesskaps rettigheter "%s" lagt til/oppdatert for "%s"';
+$lang['community permissions "%s" removed for "%s"'] = 'fellesskaps rettigheter "%s" fjernet for "%s"';
+?>
