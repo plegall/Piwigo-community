1273a124ba449b01a9f36ab80219ec76d30579c4
samli <samli@piwigo.org>
2011-02-27 02:28:53 +0000

    [extensions] [community] add HU language
    git-svn-id: http://piwigo.org/svn/extensions/community@9391 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Engedély hozzáadása';
+$lang['Who?'] = 'Ki?';
+$lang['any visitor'] = 'minden látogató';
+$lang['any registered user'] = 'regisztrált felhasználók';
+$lang['a specific user'] = 'konkrét felhasználó';
+$lang['a group'] = 'csoport';
+$lang['Where?'] = 'Hová?';
+$lang['The whole gallery'] = 'Teljes galéria';
+$lang['ability to create sub-albums'] = 'létrehozhat belső albumot';
+$lang['Which level of trust?'] = 'Felhasználói szint?';
+$lang['sub-albums creation'] = 'belső album létrehozása';
+$lang['Upload Permissions'] = 'Engedélyek feltöltése';
+$lang['Pending Photos'] = 'Függőben lévő képek';
+$lang['Permission added'] = 'Engedély hozzáadva';
+$lang['Permission removed'] = 'Engedély törölve';
+$lang['%s (the user)'] = '%s (a felhasználó)';
+$lang['%s (the group)'] = '%s (a csoport)';
+$lang['low trust'] = 'alacsony jogosultság';
+$lang['uploaded photos must be validated by an administrator'] = 'a feltöltött képeket jóvá kell hagynia az adminisztrátornak';
+$lang['high trust'] = 'magas jogosultság';
+$lang['uploaded photos are directly displayed in the gallery'] = 'a feltöltött képek azonnal megjelennek a galériában';
+$lang['%d photos validated'] = '%d kép elfogadva';
+$lang['%d photos rejected'] = '%d kép elutasítva';
+$lang['web size'] = 'web méret';
+$lang['Zoom'] = 'Zoom';
+$lang['Upload your own photos'] = 'Töltsd fel a képeid';
+$lang['%d photos uploaded by %s'] = '%d kép %s által feltöltve';
+$lang['Validation page: %s'] = 'Érvényesítés oldal: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d kép feltöltve a(z) "%s albumba"';
+$lang['Hi administrators,'] = 'Szia adminisztrátor,';
+?>
