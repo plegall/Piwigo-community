20b93969c522c29d166b34e950127f1c6cde2c24
ddtddt <ddtddt@piwigo.org>
2011-02-27 16:40:23 +0000

    [extensions] - community - add sv-SE thanks to jonas
    git-svn-id: http://piwigo.org/svn/extensions/community@9399 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Lägg till en behörighetsnivå';
+$lang['Who?'] = 'Vem?';
+$lang['any visitor'] = 'alla besökare';
+$lang['any registered user'] = 'alla registrerade användare';
+$lang['a specific user'] = 'en viss användare';
+$lang['a group'] = 'en grupp';
+$lang['Where?'] = 'Var?';
+$lang['The whole gallery'] = 'Hela galleriet';
+$lang['ability to create sub-albums'] = 'förmåga att skapa underliggande album';
+$lang['Which level of trust?'] = 'Vilken nivå av förtroende?';
+$lang['sub-albums creation'] = 'under album skapande';
+$lang['Upload Permissions'] = 'Behörigheter för att ladda upp';
+$lang['Pending Photos'] = 'Väntande bilder';
+$lang['Permission added'] = 'Behörighet läggas till';
+$lang['Permission removed'] = 'Behörighet borttagen';
+$lang['%s (the user)'] = '%s (användaren)';
+$lang['%s (the group)'] = '%s (gruppen)';
+$lang['low trust'] = 'lågt förtroende';
+$lang['uploaded photos must be validated by an administrator'] = 'uppladdade bilder måste godkännas av en administratör';
+$lang['high trust'] = 'högt förtroende';
+$lang['uploaded photos are directly displayed in the gallery'] = 'uppladdade bilder visas direkt i galleriet';
+$lang['%d photos validated'] = '%d godkända bilder';
+$lang['%d photos rejected'] = '%d bilder avvisas';
+$lang['web size'] = 'web storlek';
+$lang['Zoom'] = 'Zoom';
+$lang['Upload your own photos'] = 'Ladda upp dina egna bilder';
+$lang['%d photos uploaded by %s'] = '%d bilder uppladdade av %s';
+$lang['Validation page: %s'] = 'Validering sida: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d bilder uppladdade till albumet "%s"';
+$lang['Hi administrators,'] = 'Hej administratörer,';
+?>
