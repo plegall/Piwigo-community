d6341c3a8782ceccbd1e2e33e557fb80e14a36ea
ddtddt <ddtddt@piwigo.org>
2011-02-28 21:26:40 +0000

    [extensions] - community - add sk_SK thanks to dodo steap 1
    git-svn-id: http://piwigo.org/svn/extensions/community@9434 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Pridaj oprávnenie'; 
+$lang['Who?'] = 'Kto?'; 
+$lang['any visitor'] = 'každý návštevník'; 
+$lang['any registered user'] = 'každý registrovaný používateľ'; 
+$lang['a specific user'] = 'špecifikovaný používateľ'; 
+$lang['a group'] = 'skupina'; 
+$lang['Where?'] = 'Kde?'; 
+$lang['The whole gallery'] = 'Celá galéria'; 
+$lang['ability to create sub-albums'] = 'schopnosť vytvoriť podalbumy'; 
+$lang['Which level of trust?'] = 'Ktorá úroveň dôvery?'; 
+$lang['sub-albums creation'] = 'vytváranie podalbumov'; 
+$lang['Upload Permissions'] = 'Nahrať oprávnenia'; 
+$lang['Pending Photos'] = 'Čakajúce fotografie'; 
+$lang['Permission added'] = 'Oprávnenia pridané'; 
+$lang['Permission removed'] = 'Oprávnenia odstránené'; 
+$lang['%s (the user)'] = '%s (používateľ)'; 
+$lang['%s (the group)'] = '%s (skupina)'; 
+$lang['low trust'] = 'nízka dôvera'; 
+$lang['uploaded photos must be validated by an administrator'] = 'nahraté fotografie musia byť schválené administrátorom'; 
+$lang['high trust'] = 'vysoká dôvera'; 
+$lang['uploaded photos are directly displayed in the gallery'] = 'nahraté fotografie sú priamo zobrazené v galérii'; 
+$lang['%d photos validated'] = '%d fotografií potvrdených'; 
+$lang['%d photos rejected'] = '%d fotografií odmietnutých'; 
+$lang['web size'] = 'veľkosť webu'; 
+$lang['Zoom'] = 'Zväčšenie'; 
+$lang['Upload your own photos'] = 'Nahrať Vaše vlastné fotografie'; 
+$lang['%d photos uploaded by %s'] = '%d fotografií nahratých %s'; 
+$lang['Validation page: %s'] = 'Potvrdzovacia stránka: %s'; 
+$lang['%d photos uploaded into album "%s"'] = '%d fotografií nahratých do albumu "%s"'; 
+$lang['Hi administrators,'] = 'Ahojte administrátori,';
+
+?>
