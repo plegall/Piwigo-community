39a8141e8a05611dad28805fe565dc04abcc5620
ddtddt <ddtddt@piwigo.org>
2011-02-28 21:28:45 +0000

    [extensions] - community - add DE and IT thanks to sugar888
    git-svn-id: http://piwigo.org/svn/extensions/community@9435 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Eine Berechtigung hinzufügen';
+$lang['Who?'] = 'Wer?';
+$lang['any visitor'] = 'jegliche Besucher';
+$lang['any registered user'] = 'Jeglicher registrierter Benutzer';
+$lang['a specific user'] = 'ein spezifischer Benutzer';
+$lang['a group'] = 'eine Gruppe';
+$lang['Where?'] = 'Wo?';
+$lang['The whole gallery'] = 'Die ganze Galerie';
+$lang['ability to create sub-albums'] = 'Fähigkeit zum Erstellen von Unteralben';
+$lang['Which level of trust?'] = 'Welcher Grad an Vertrauen?';
+$lang['sub-albums creation'] = 'Erstellung der Unteralben';
+$lang['Upload Permissions'] = 'Berechtigung zum Upload';
+$lang['Pending Photos'] = 'Ausstehende Fotos';
+$lang['Permission added'] = 'Berechtigung hinzugefügt';
+$lang['Permission removed'] = 'Berechtigung entfernt';
+$lang['%s (the user)'] = '%s (der Benutzer)';
+$lang['%s (the group)'] = '%s (die Gruppe)';
+$lang['low trust'] = 'geringes Vertrauen';
+$lang['uploaded photos must be validated by an administrator'] = 'Hochgeladene Fotos müssen vom Administrator validiert werden';
+$lang['high trust'] = 'hohes Vertrauen';
+$lang['uploaded photos are directly displayed in the gallery'] = 'Hochgeladen Fotos werden direkt in der Galerie angezeigt';
+$lang['%d photos validated'] = '%d Fotos validiert';
+$lang['%d photos rejected'] = '%d Fotos abgelehnt';
+$lang['web size'] = 'Web-Größe';
+$lang['Zoom'] = 'Zoom';
+$lang['Upload your own photos'] = 'Laden Sie Ihre eigenen Fotos hoch';
+$lang['%d photos uploaded by %s'] = '%d Fotos hochgeladen von %s';
+$lang['Validation page: %s'] = 'Validierungsseite: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d Fotos hochgeladen im Album "%s"';
+$lang['Hi administrators,'] = 'Hallo Administratoren,';
+
+?>
---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Aggiungere un permesso';
+$lang['Who?'] = 'Chi?';
+$lang['any visitor'] = 'qualsiasi visitatore';
+$lang['any registered user'] = 'qualsiasi utente registrato';
+$lang['a specific user'] = 'un utente specifico';
+$lang['a group'] = 'un gruppo';
+$lang['Where?'] = 'Dove?';
+$lang['The whole gallery'] = 'Tutta la galleria';
+$lang['ability to create sub-albums'] = 'abilità di creare un sub album';
+$lang['Which level of trust?'] = 'Quale livello di fiducia?';
+$lang['sub-albums creation'] = 'creazione dei sub album';
+$lang['Upload Permissions'] = 'Permesso per l\'upload';
+$lang['Pending Photos'] = 'Foto pendenti';
+$lang['Permission added'] = 'Permesso aggiunto';
+$lang['Permission removed'] = 'Permesso rimosso';
+$lang['%s (the user)'] = '%s (l\'utente)';
+$lang['%s (the group)'] = '%s (il gruppo)';
+$lang['low trust'] = 'bassa affidabilità';
+$lang['uploaded photos must be validated by an administrator'] = 'le foto caricate devono essere validate da un amministratore';
+$lang['high trust'] = 'alta affidabilità';
+$lang['uploaded photos are directly displayed in the gallery'] = 'le foto caricate sono direttamente visualizzate nella galleria';
+$lang['%d photos validated'] = '%d foto validate';
+$lang['%d photos rejected'] = '%d foto respinte';
+$lang['web size'] = 'formato web';
+$lang['Zoom'] = 'Zoom';
+$lang['Upload your own photos'] = 'Caricate le vostre foto';
+$lang['%d photos uploaded by %s'] = '%d foto caricate da %s';
+$lang['Validation page: %s'] = 'Pagina di validazione: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d le foto caricate nell\'album "%s"';
+$lang['Hi administrators,'] = 'Salve amministratori,';
+
+?>
