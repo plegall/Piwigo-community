4fc4a372adc67d811ee36f07ffdc3f9074e7e91a
ddtddt <ddtddt@piwigo.org>
2011-03-01 16:03:57 +0000

    [extensions] - community - add es_ES thanks to jpr928
    git-svn-id: http://piwigo.org/svn/extensions/community@9460 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Añadir un permiso';
+$lang['Who?'] = 'Quien ?';
+$lang['any visitor'] = 'cualquier visitante';
+$lang['any registered user'] = 'cualquier visitante registrado';
+$lang['a specific user'] = 'un usuario en particular';
+$lang['a group'] = 'un grupo';
+$lang['Where?'] = 'Donde ?';
+$lang['The whole gallery'] = 'Toda la galeria';
+$lang['ability to create sub-albums'] = 'posibilidad de crear sub-álbumes';
+$lang['Which level of trust?'] = '¿Qué grado de confianza ?';
+$lang['sub-albums creation'] = 'creación de sub-álbumes';
+$lang['Upload Permissions'] = 'Permisos para agregar';
+$lang['Pending Photos'] = 'Fotos pendientes';
+$lang['Permission added'] = 'Permisos registrados';
+$lang['Permission removed'] = 'Permiso eliminado';
+$lang['%s (the user)'] = '%s (El usuario)';
+$lang['%s (the group)'] = '%s (el grupo)';
+$lang['low trust'] = 'confianza fiable';
+$lang['uploaded photos must be validated by an administrator'] = 'las fotos añadidas debe ser validadas por un administrador';
+$lang['high trust'] = 'confianza alta';
+$lang['uploaded photos are directly displayed in the gallery'] = 'las fotos añadidas son visibles directamente en la galería';
+$lang['%d photos validated'] = '%d fotos validadas';
+$lang['%d photos rejected'] = '%d fotos rechazadas';
+$lang['web size'] = 'tamaño web';
+$lang['Zoom'] = 'Zoom';
+$lang['Upload your own photos'] = 'Añade tus propias fotos';
+$lang['%d photos uploaded by %s'] = '%d fotos añadidas por %s';
+$lang['Validation page: %s'] = 'página de validación : %s';
+$lang['%d photos uploaded into album "%s"'] = '%d fotos añadidas al álbum "%s"';
+$lang['Hi administrators,'] = 'Buenos dias queridos administradores,';
+
+?>
