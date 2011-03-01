5a9b23a8e26abd8783bbb731ae898dbb7ba21cf3
ddtddt <ddtddt@piwigo.org>
2011-03-01 18:42:00 +0000

    [extensions] - community - add cs_CZ thanks to webprostor.eu
    git-svn-id: http://piwigo.org/svn/extensions/community@9465 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Přidat povolení';
+$lang['Who?'] = 'Komu?';
+$lang['any visitor'] = 'každém návštěvníkovi';
+$lang['any registered user'] = 'každému registrovanému uživateli';
+$lang['a specific user'] = 'konkrétnímu uživatele';
+$lang['a group'] = 'skupině';
+$lang['Where?'] = 'Kde?';
+$lang['The whole gallery'] = 'celou galerii';
+$lang['ability to create sub-albums'] = 'povolit vytváření sub-alb';
+$lang['Which level of trust?'] = 'Kterou úroveň oprávnění?';
+$lang['sub-albums creation'] = 'vytvoření sub-alb';
+$lang['Upload Permissions'] = 'Povoloení pro Upload';
+$lang['Pending Photos'] = 'Čekající fotografie';
+$lang['Permission added'] = 'Povolení přidáno';
+$lang['Permission removed'] = 'Povolení odebráno';
+$lang['%s (the user)'] = '%s (uživatel)';
+$lang['%s (the group)'] = '%s (skupina)';
+$lang['low trust'] = 'nízká důvěra';
+$lang['uploaded photos must be validated by an administrator'] = 'uploadované fotky musí být schváleny administrátorem';
+$lang['high trust'] = 'vysoká důvěra';
+$lang['uploaded photos are directly displayed in the gallery'] = 'uploadované fotky jsou ihned zobrazeny ve fotogalerii';
+$lang['%d photos validated'] = '%d fotek schváleno';
+$lang['%d photos rejected'] = '%d fotek nechváleno';
+$lang['web size'] = 'web velikost';
+$lang['Zoom'] = 'Zoom';
+$lang['Upload your own photos'] = 'Nahraj vlastní fotky';
+$lang['%d photos uploaded by %s'] = '%d obrázků náhrál %s';
+$lang['Validation page: %s'] = 'Ověřovací stránka: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d fotek nahráno do alba "%s"';
+$lang['Hi administrators,'] = 'Ahoj administrátoři,';
+?>
