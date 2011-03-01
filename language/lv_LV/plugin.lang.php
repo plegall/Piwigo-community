9d3922b58d97deaf1783ee8f5515a27c82b8c2e1
ddtddt <ddtddt@piwigo.org>
2011-03-01 17:11:59 +0000

    [extensions] - community - add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/community@9461 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Pievienot atļauju';
+$lang['Who?'] = 'Kas?';
+$lang['any visitor'] = 'jebkurš lietotājs';
+$lang['any registered user'] = 'jebkurš reģistrēts lietotājs';
+$lang['a specific user'] = 'īpašs lietotājs';
+$lang['a group'] = 'grupa';
+$lang['Where?'] = 'Kur?';
+$lang['The whole gallery'] = 'Visa galerija';
+$lang['ability to create sub-albums'] = 'spēja veidot apakšalbūmus';
+$lang['Which level of trust?'] = 'Kurš uzticības līmenis?';
+$lang['sub-albums creation'] = 'apakšalbūmu izveide';
+$lang['Upload Permissions'] = 'Augšupielādēt Atļaujas';
+$lang['Pending Photos'] = 'Gaidāmās Fotogrāfijas';
+$lang['Permission added'] = 'Atļaujas pievienotas';
+$lang['Permission removed'] = 'Atļauja noņemta';
+$lang['%s (the user)'] = '%s (lietotājs)';
+$lang['%s (the group)'] = '%s (grupa)';
+$lang['low trust'] = 'zems uzticības līmenis';
+$lang['uploaded photos must be validated by an administrator'] = 'administratoram jāvalidē augšupielādētās fotogrāfijas';
+$lang['high trust'] = 'augsts uzticības līmenis';
+$lang['uploaded photos are directly displayed in the gallery'] = 'augšupielādētās fotogrāfijas pa tiešo tiks parādītas galerijā';
+$lang['%d photos validated'] = '%d fotogrāfijas validētas';
+$lang['%d photos rejected'] = '%d fotogrāfijas noreidītas';
+$lang['web size'] = 'tīkla izmērs';
+$lang['Zoom'] = 'Tālummaiņa';
+$lang['Upload your own photos'] = 'Augšupielādējiet savas fotogrāfijas';
+$lang['%d photos uploaded by %s'] = '%d fotogrāfijas augšupielādētas ar %s';
+$lang['Validation page: %s'] = 'Validācijas lapa: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d fotogrāfijas augšupielādētas albūmā "%s"';
+$lang['Hi administrators,'] = 'Sveiki administratori,';
+
+?>
