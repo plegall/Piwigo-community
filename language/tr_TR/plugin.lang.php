62885bb82dbf4b8a712f7ba2c6c3fca9a1320445
ddtddt <ddtddt@piwigo.org>
2011-03-02 17:01:55 +0000

    [extensions] - community - add tr_TR thanks to nakre
    git-svn-id: http://piwigo.org/svn/extensions/community@9481 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'İzin Ekle';
+$lang['Who?'] = 'Kim?';
+$lang['any visitor'] = 'herhangi bir ziyaretçi';
+$lang['any registered user'] = 'herhangi bir kayıtlı kullanıcı';
+$lang['a specific user'] = 'belirli bir kullanıcı';
+$lang['a group'] = 'bir grup';
+$lang['Where?'] = 'Nerede?';
+$lang['The whole gallery'] = 'tüm galeride';
+$lang['ability to create sub-albums'] = 'alt albüm oluşturma izni';
+$lang['Which level of trust?'] = 'Güven seviyesi?';
+$lang['sub-albums creation'] = 'alt albüm oluşturma';
+$lang['Upload Permissions'] = 'Yükleme İzinleri';
+$lang['Pending Photos'] = 'Bekleyen Resimler';
+$lang['Permission added'] = 'İzin eklendi';
+$lang['Permission removed'] = 'İzin silindi';
+$lang['%s (the user)'] = '%s (kullanıcısına)';
+$lang['%s (the group)'] = '%s (grubuna)';
+$lang['low trust'] = 'düşük güven';
+$lang['uploaded photos must be validated by an administrator'] = 'yüklenen resimler bir yönetici tarafından onaylanmalıdır';
+$lang['high trust'] = 'yüksek güven';
+$lang['uploaded photos are directly displayed in the gallery'] = 'yüklenen resimler doğrudan galeride sergilensin';
+$lang['%d photos validated'] = '%d resimler onaylandı';
+$lang['%d photos rejected'] = '%d resimler reddedildi';
+$lang['web size'] = 'web\'deki boyutu';
+$lang['Zoom'] = 'Yakınlaştırma';
+$lang['Upload your own photos'] = 'Kendi resimlerinizi yükleyin';
+$lang['%d photos uploaded by %s'] = '%d resimler %s tarafından yüklendi';
+$lang['Validation page: %s'] = 'Onaylama sayfası: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d resimler "%s" albümüne yüklendi';
+$lang['Hi administrators,'] = 'Merhaba yöneticiler,';
+
+/*TODO*/$lang['No album, this photo is orphan'] = 'No album, this photo is orphan';
+?>
