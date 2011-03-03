bcdc14dfea8ae0b1d5eb51f629675a4b7311eeef
samli <samli@piwigo.org>
2011-03-03 07:09:38 +0000

    [extensions] [community] modified HU language
    git-svn-id: http://piwigo.org/svn/extensions/community@9489 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/plugin.lang.php
+++ language/hu_HU/plugin.lang.php
@@ -49,4 +49,6 @@ $lang['%d photos uploaded by %s'] = '%d kép %s által feltöltve';
 $lang['Validation page: %s'] = 'Érvényesítés oldal: %s';
 $lang['%d photos uploaded into album "%s"'] = '%d kép feltöltve a(z) "%s albumba"';
 $lang['Hi administrators,'] = 'Szia adminisztrátor,';
+
+$lang['No album, this photo is orphan'] = 'Nincs album, ez a kép árva';
 ?>
