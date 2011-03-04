0b2c06cdc7b2ae2047332418748a8a7b0b44c7f7
ddtddt <ddtddt@piwigo.org>
2011-03-04 07:26:13 +0000

    [extensions] - community - add no_NO (Norwegian) thanks to atlew
    git-svn-id: http://piwigo.org/svn/extensions/community@9508 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/no_NO/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Legg til en rettighet'; 
+$lang['Who?'] = 'Hvem?'; 
+$lang['any visitor'] = 'hvem som helst besøkende'; 
+$lang['any registered user'] = 'hvem som helst registrert bruker'; 
+$lang['a specific user'] = 'en spesifik bruker'; 
+$lang['a group'] = 'en gruppe'; 
+$lang['Where?'] = 'Hvor?'; 
+$lang['The whole gallery'] = 'Hele galleriet'; 
+$lang['ability to create sub-albums'] = 'mulighet til å lage under album'; 
+$lang['Which level of trust?'] = 'Hvilket tillits nivå?'; 
+$lang['sub-albums creation'] = 'under albums lager'; 
+$lang['Upload Permissions'] = 'Last opp rettigheter'; 
+$lang['Pending Photos'] = 'Ventende bilder'; 
+$lang['Permission added'] = 'Rettigheter lagt til'; 
+$lang['Permission removed'] = 'Rettigheter fjernet';
+$lang['%s (the user)'] = '%s (brukeren)'; 
+$lang['%s (the group)'] = '%s (gruppen)'; 
+$lang['low trust'] = 'lav tillit'; 
+$lang['uploaded photos must be validated by an administrator'] = 'opplastede bilder må bli validert av en administrator'; 
+$lang['high trust'] = 'høy tillit'; 
+$lang['uploaded photos are directly displayed in the gallery'] = 'opplastede bilder bil vist direkte i galleriet'; 
+$lang['%d photos validated'] = '%d bilder validert'; 
+$lang['%d photos rejected'] = '%d bilder avvist'; 
+$lang['web size'] = 'web størrelse'; 
+$lang['Zoom'] = 'Zoom'; 
+$lang['Upload your own photos'] = 'Last opp dine egne bilder'; 
+$lang['%d photos uploaded by %s'] = '%d bilder opplastet av %s'; 
+$lang['Validation page: %s'] = 'Validering side: %s'; 
+$lang['%d photos uploaded into album "%s"'] = '%d bilder lastet opp til album "%s"'; 
+$lang['Hi administrators,'] = 'Hei administratorer,';
+
+/*TODO*/$lang['No album, this photo is orphan'] = 'No album, this photo is orphan';
+?>
