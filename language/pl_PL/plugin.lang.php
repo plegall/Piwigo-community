eb1fe5692f681aedc9b37270b03f1d773961816e
ddtddt <ddtddt@piwigo.org>
2011-03-15 20:18:32 +0000

    [extensions] - community - add PL thanks to  voyteckst step1
    git-svn-id: http://piwigo.org/svn/extensions/community@9715 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,55 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Dodaj uprawnienia';
+$lang['Who?'] = 'Kto?';
+$lang['any visitor'] = 'dowolny odwiedzający';
+$lang['any registered user'] = 'dowolny zarejestrowany użytkownik';
+$lang['a specific user'] = 'konkretny użytkownik';
+$lang['a group'] = 'grupa';
+$lang['Where?'] = 'Gdzie?';
+$lang['The whole gallery'] = 'Cała galeria';
+$lang['ability to create sub-albums'] = 'możliwość tworzenia pod-albumów';
+$lang['Which level of trust?'] = 'Który poziom zufania?';
+$lang['sub-albums creation'] = 'tworzenie pod-albumów';
+$lang['Upload Permissions'] = 'Uprawnienia Wgrywania';
+$lang['Pending Photos'] = 'Oczekujące Zdjęcia';
+$lang['Permission added'] = 'Dodane uprawnienia';
+$lang['Permission removed'] = 'Usunięte uprawnienia';
+$lang['%s (the user)'] = '%s (użytkownik)';
+$lang['%s (the group)'] = '%s (grupa)';
+$lang['low trust'] = 'niskie zaufanie';
+$lang['uploaded photos must be validated by an administrator'] = 'wgrane zdjęcia muszą być zatwierdzone przez administratora';
+$lang['high trust'] = 'wysokie zaufanie';
+$lang['uploaded photos are directly displayed in the gallery'] = 'wgrane zdjęcia są od razu wyświetlane w galerii';
+$lang['%d photos validated'] = '%d zdjęć zaakceptowanych';
+$lang['%d photos rejected'] = '%d zdjęć odrzuconych';
+$lang['web size'] = 'rozmiar web';
+$lang['Zoom'] = 'Powiększenie';
+$lang['Upload your own photos'] = 'Wgraj swoje własne zdjęcia';
+$lang['%d photos uploaded by %s'] = '%d zdjęć wgranych przez %s';
+$lang['Validation page: %s'] = 'Strona walidacji: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d zdjęć wgranych do albumu "%s"';
+$lang['Hi administrators,'] = 'Witajcie administratorzy,';
+
+?>
