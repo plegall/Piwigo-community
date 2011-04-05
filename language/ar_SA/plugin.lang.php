954accfd5668e9d750ca1c95b4e5ca69fef112fa
ddtddt <ddtddt@piwigo.org>
2011-04-05 17:11:12 +0000

    [extensions] - community - add ar_SA thanks to bakir
    git-svn-id: http://piwigo.org/svn/extensions/community@10077 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ar_SA/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'اضافة صلاحية';
+$lang['Who?'] = 'من؟';
+$lang['any visitor'] = 'اي زائر';
+$lang['any registered user'] = 'اي عضو مسجل';
+$lang['a specific user'] = 'مستخدم معين';
+$lang['a group'] = 'مجموعة';
+$lang['Where?'] = 'اين؟';
+$lang['The whole gallery'] = 'كل النعرض';
+$lang['ability to create sub-albums'] = 'القدرة على خلق التصنيفات الفرعية';
+$lang['Which level of trust?'] = 'اي درجة من الثقة؟';
+$lang['sub-albums creation'] = 'خلق التصنيفات الفرعية';
+$lang['Upload Permissions'] = 'صلاحيات الرفع';
+$lang['Pending Photos'] = 'الصور في قائمة الانتظار';
+$lang['Permission added'] = 'تم اضافة الصلاحيات';
+$lang['Permission removed'] = 'تم ازالة الصلاحيات';
+$lang['%s (the user)'] = '%s (المستخدم)';
+$lang['%s (the group)'] = '%s (المجموعة)';
+$lang['low trust'] = 'ثقة منخفضة';
+$lang['uploaded photos must be validated by an administrator'] = 'الصور المرفوعة يجب التحقق منها من قبل الادارة';
+$lang['high trust'] = 'ثقة عالية';
+$lang['uploaded photos are directly displayed in the gallery'] = 'الصور المرفوعة تعرض مباشرة في المعرض';
+$lang['%d photos validated'] = '%d صور تم التحقق منها';
+$lang['%d photos rejected'] = '%d صور رفضت';
+$lang['web size'] = 'حجم الويب';
+$lang['Zoom'] = 'التقريب';
+$lang['Upload your own photos'] = 'ارفع صورك الشخصية';
+$lang['%d photos uploaded by %s'] = '%d صور رفعت من قبل %s';
+$lang['Validation page: %s'] = 'صفحة التحقق: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d صور رفعت في الالبوم"%s"';
+$lang['Hi administrators,'] = 'مرحبا الادارة,';
+
+$lang['No album, this photo is orphan'] = 'لا البوم , هذه الصورة وحيدة';
+
+$lang['Cancel'] = 'الغاء';
+$lang['Permission updated'] = 'الصلاحيات حدثت';
+$lang['Delete permission'] = 'حذف الصلاحية';
+$lang['%u pending photos'] = '%u صور بالانتظار';
+$lang['Level 16'] = 'انتظار';
+?>
