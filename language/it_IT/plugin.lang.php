882c971ceec211a4d2081062b30727099e5ff94d
riopwg <riopwg@piwigo.org>
2011-04-11 08:55:21 +0000

    Update language IT
    git-svn-id: http://piwigo.org/svn/extensions/community@10284 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -2,7 +2,9 @@
 // +-----------------------------------------------------------------------+
 // | Piwigo - a PHP based picture gallery                                  |
 // +-----------------------------------------------------------------------+
-// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
 // +-----------------------------------------------------------------------+
 // | This program is free software; you can redistribute it and/or modify  |
 // | it under the terms of the GNU General Public License as published by  |
@@ -27,27 +29,33 @@ $lang['a specific user'] = 'un utente specifico';
 $lang['a group'] = 'un gruppo';
 $lang['Where?'] = 'Dove?';
 $lang['The whole gallery'] = 'Tutta la galleria';
-$lang['ability to create sub-albums'] = 'abilità di creare un sub album';
+$lang['ability to create sub-albums'] = 'abilitare la creazione di sub-album';
 $lang['Which level of trust?'] = 'Quale livello di fiducia?';
-$lang['sub-albums creation'] = 'creazione dei sub album';
-$lang['Upload Permissions'] = 'Permesso per l\'upload';
-$lang['Pending Photos'] = 'Foto pendenti';
-$lang['Permission added'] = 'Permesso aggiunto';
+$lang['sub-albums creation'] = 'creazione dei sub-album';
+$lang['Upload Permissions'] = 'Autorizzazione per l\'upload';
+$lang['Pending Photos'] = 'Foto in attesa';
+$lang['Permission added'] = 'Permesso registrato';
 $lang['Permission removed'] = 'Permesso rimosso';
 $lang['%s (the user)'] = '%s (l\'utente)';
 $lang['%s (the group)'] = '%s (il gruppo)';
-$lang['low trust'] = 'bassa affidabilità';
-$lang['uploaded photos must be validated by an administrator'] = 'le foto caricate devono essere validate da un amministratore';
-$lang['high trust'] = 'alta affidabilità';
-$lang['uploaded photos are directly displayed in the gallery'] = 'le foto caricate sono direttamente visualizzate nella galleria';
-$lang['%d photos validated'] = '%d foto validate';
-$lang['%d photos rejected'] = '%d foto respinte';
+$lang['low trust'] = 'bassa fiducia';
+$lang['uploaded photos must be validated by an administrator'] = 'le foto caricate devono essere convalidate da un amministratore';
+$lang['high trust'] = 'alta fiducia';
+$lang['uploaded photos are directly displayed in the gallery'] = 'le foto caricate sono direttamente visibili nella galleria';
+$lang['%d photos validated'] = '%d foto convalidate';
+$lang['%d photos rejected'] = '%d foto rifiutate';
 $lang['web size'] = 'formato web';
 $lang['Zoom'] = 'Zoom';
 $lang['Upload your own photos'] = 'Caricate le vostre foto';
 $lang['%d photos uploaded by %s'] = '%d foto caricate da %s';
-$lang['Validation page: %s'] = 'Pagina di validazione: %s';
+$lang['Validation page: %s'] = 'Pagina di convalida: %s';
 $lang['%d photos uploaded into album "%s"'] = '%d le foto caricate nell\'album "%s"';
 $lang['Hi administrators,'] = 'Salve amministratori,';
 
+$lang['No album, this photo is orphan'] = 'Nessun album, questa photo è orfana';
+$lang['Cancel'] = 'Cancellare';
+$lang['Permission updated'] = 'Autorizzazione a l\'aggiornamento';
+$lang['Delete permission'] = 'Cancellare il permesso';
+$lang['%u pending photos'] = '%u foto in attesa';
+$lang['Level 16'] = 'In attesa';
 ?>
