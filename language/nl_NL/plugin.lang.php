a77bdd8c32753ac32cfdc9d08d91bf2b9a07705a
ddtddt <ddtddt@piwigo.org>
2011-04-16 13:23:32 +0000

    [extensions] - community - add NL Thanks J.Commelin
    git-svn-id: http://piwigo.org/svn/extensions/community@10417 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Voeg een permissie toe';
+$lang['Who?'] = 'Wie?';
+$lang['any visitor'] = 'iedere bezoeker';
+$lang['any registered user'] = 'ieder geregistreerde gebruiker';
+$lang['a specific user'] = 'een specifieke gebruiker';
+$lang['a group'] = 'een groep';
+$lang['Where?'] = 'Waar?';
+$lang['The whole gallery'] = 'De hele galerij';
+$lang['ability to create sub-albums'] = 'mogelijkheid om subalbums te maken';
+$lang['Which level of trust?'] = 'Welk niveau van vertrouwen?';
+$lang['sub-albums creation'] = 'het aanmaken van subalbums ';
+$lang['Upload Permissions'] = 'Upload Permissies';
+$lang['Pending Photos'] = 'Foto\'s in de wachtrij';
+$lang['Permission added'] = 'Permissie toegevoegd';
+$lang['Permission removed'] = 'Permissie verwijderd';
+$lang['%s (the user)'] = '%s (de gebruiker)';
+$lang['%s (the group)'] = '%s (de groep)';
+$lang['low trust'] = 'laag vertrouwen';
+$lang['uploaded photos must be validated by an administrator'] = 'geüploade foto\'s moeten worden bevestigd door een administrator';
+$lang['high trust'] = 'hoog vertrouwen';
+$lang['uploaded photos are directly displayed in the gallery'] = 'geüploade foto\'s worden meteen weergegeven in de galerij';
+$lang['%d photos validated'] = '%d foto\'s bevestigd';
+$lang['%d photos rejected'] = '%d foto\'s geweigerd';
+$lang['web size'] = 'web formaat';
+$lang['Zoom'] = 'Zoom';
+$lang['Upload your own photos'] = 'Upload uw eigen foto\'s';
+$lang['%d photos uploaded by %s'] = '%d foto\'s geüpload door %s';
+$lang['Validation page: %s'] = 'Bevestigingspagina: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d foto\'s geüpload in album "%s"';
+$lang['Hi administrators,'] = 'Hoi administrators,';
+
+$lang['No album, this photo is orphan'] = 'Geen album, deze foto is wees';
+
+$lang['Cancel'] = 'Annuleer';
+$lang['Permission updated'] = 'Permissie geüpdate';
+$lang['Delete permission'] = 'Verwijder permissie';
+$lang['%u pending photos'] = '%u foto\'s in de wachtrij';
+$lang['Level 16'] = 'In de wachtrij';
+
+?>
