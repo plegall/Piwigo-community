0c131bdca417895a4e6aca19e3ff41bd8ae41f24
ddtddt <ddtddt@piwigo.org>
2011-04-16 12:15:08 +0000

    [extensions] - community - add vi_VN Thanks Em Ku
    git-svn-id: http://piwigo.org/svn/extensions/community@10413 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Thêm quyền hạn';
+$lang['Who?'] = 'Ai?';
+$lang['any visitor'] = 'bất kỳ khách thăm quan nào';
+$lang['any registered user'] = 'bất kỳ người dùng đã đăng ký nào';
+$lang['a specific user'] = 'một người dùng cụ thể';
+$lang['a group'] = 'một nhóm';
+$lang['Where?'] = 'Ở đâu?';
+$lang['The whole gallery'] = 'Toàn bộ thư viện hình';
+$lang['ability to create sub-albums'] = 'khả năng tạo album con';
+$lang['Which level of trust?'] = 'Độ tin cậy mức độ nào?';
+$lang['sub-albums creation'] = 'tạo album con';
+$lang['Upload Permissions'] = 'Quyền uload hình';
+$lang['Pending Photos'] = 'Hình đang chờ xử lý';
+$lang['Permission added'] = 'Quyền hạn đã được thêm';
+$lang['Permission removed'] = 'Quyền hạn đã được gỡ bỏ';
+$lang['%s (the user)'] = '%s (người dùng)';
+$lang['%s (the group)'] = '%s (nhóm)';
+$lang['low trust'] = 'độ tin cậy thấp';
+$lang['uploaded photos must be validated by an administrator'] = 'những hình đã tải lên phải được thông qua bởi quản trị viên';
+$lang['high trust'] = 'độ tin cậy cao';
+$lang['uploaded photos are directly displayed in the gallery'] = 'những hình đã tải lên được hiển trị trực tiếp trên thư viện ảnh';
+$lang['%d photos validated'] = 'có %d ảnh đã được thông qua';
+$lang['%d photos rejected'] = 'có %d ảnh đã bị loại bỏ';
+$lang['web size'] = 'kích cỡ trang web';
+$lang['Zoom'] = 'Phóng to ảnh';
+$lang['Upload your own photos'] = 'Tải ảnh riêng của bạn';
+$lang['%d photos uploaded by %s'] = 'có %d ảnh đã được tải lên bởi %s';
+$lang['Validation page: %s'] = 'Trang thông qua: %s';
+$lang['%d photos uploaded into album "%s"'] = 'có %d ảnh đã được tải lên vào album "%s"';
+$lang['Hi administrators,'] = 'Xin chào quản trị viên,';
+
+$lang['No album, this photo is orphan'] = 'Không có album nào, ảnh này là ảnh đơn';
+
+$lang['Cancel'] = 'Hủy bỏ';
+$lang['Permission updated'] = 'Quyền hạn đã được cập nhật';
+$lang['Delete permission'] = 'Xóa quyền hạn';
+$lang['%u pending photos'] = 'có %u ảnh đang chờ xử lý';
+$lang['Level 16'] = 'Đang chờ xử lý';
+
+?>
