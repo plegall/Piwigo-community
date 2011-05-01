b710d0849642f7fefa10dbe6f7f9845893c3e57f
ddtddt <ddtddt@piwigo.org>
2011-05-01 13:28:55 +0000

    [extensions] - community - update SK thanks to dodo
    git-svn-id: http://piwigo.org/svn/extensions/community@10723 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -55,5 +55,7 @@ $lang['Permission updated'] = 'Oprávnenie aktualizované';
 $lang['Delete permission'] = 'Oprávnenie vymazané';
 $lang['%u pending photos'] = '%u nevyriešených fotografií';
 $lang['Level 16'] = 'Nevyriešené';
+$lang['Edit a permission'] = 'Edit a permission';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Vaše fotografie čakajú na schválenie, administrátor ich musí schváliť';
 
 ?>
