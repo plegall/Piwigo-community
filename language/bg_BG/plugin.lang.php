c1bb381c52f63776e8bc3f4eb7104275d7981153
ddtddt <ddtddt@piwigo.org>
2011-06-29 18:33:28 +0000

    [extensions] - community - add BG (Bulgarian - Български) thanks to josifbg
    git-svn-id: http://piwigo.org/svn/extensions/community@11576 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2009      Pierrick LE GALL             http://piwigo.org |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Добави права';
+$lang['Who?'] = 'Кой?';
+$lang['any visitor'] = 'всеки посетител';
+$lang['any registered user'] = 'всеки регистриран потребител';
+$lang['a specific user'] = 'определен потребител';
+$lang['a group'] = 'група';
+$lang['Where?'] = 'Къде?';
+$lang['The whole gallery'] = 'Цялата Галерия';
+$lang['ability to create sub-albums'] = 'възможността да създава под-албуми';
+$lang['Which level of trust?'] = 'Ниво на доверие?';
+$lang['sub-albums creation'] = 'да създава под-албуми';
+$lang['Upload Permissions'] = 'Добави права';
+$lang['Pending Photos'] = 'Чакащи снимки';
+$lang['Permission added'] = 'Добавени права';
+$lang['Permission removed'] = 'Премахнати права';
+$lang['%s (the user)'] = '%s (юзер)';
+$lang['%s (the group)'] = '%s (група)';
+$lang['low trust'] = 'ниско ниво на доверие';
+$lang['uploaded photos must be validated by an administrator'] = 'добавените снимки трябва да бъдат одобрени от администратор';
+$lang['high trust'] = 'високо ниво на доверие';
+$lang['uploaded photos are directly displayed in the gallery'] = 'добавените снимки могат да се видят в галерията';
+$lang['%d photos validated'] = '%d одобрени снимки';
+$lang['%d photos rejected'] = '%d неодобрени снимки';
+$lang['web size'] = 'web size';
+$lang['Zoom'] = 'Увеличи';
+$lang['Upload your own photos'] = 'Добавете Ваши снимки';
+$lang['%d photos uploaded by %s'] = '%d снимките са добавени от %s';
+$lang['Validation page: %s'] = 'Страница за одобрение: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d снимките са добавени в албум "%s"';
+$lang['Hi administrators,'] = 'Здравейте Администратори,';
+$lang['No album, this photo is orphan'] = 'Снимка без албум';
+$lang['Cancel'] = 'отхвърли';
+$lang['Permission updated'] = 'Обновени права';
+$lang['Delete permission'] = 'Отменени права';
+$lang['%u pending photos'] = '%u чакащи снимки';
+$lang['Level 16'] = 'В очакване';
+$lang['Edit a permission'] = 'Редактиране на права';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Вашите снимки очакват одобрение, администраторите са информирани';
+?>
