67c2a17d8c5fd54d0480a516c18bd1283fcfdc27
ddtddt <ddtddt@piwigo.org>
2011-07-30 19:29:05 +0000

    [extensions] - community - add ru_RU - Русский - Russian thanks to kiddy-admin
    git-svn-id: http://piwigo.org/svn/extensions/community@11857 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,65 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Добавить разрешение';
+$lang['Who?'] = 'Кто?';
+$lang['any visitor'] = 'любой посетитель';
+$lang['any registered user'] = 'любой зарегистрированный пользователь';
+$lang['a specific user'] = 'особый пользователь';
+$lang['a group'] = 'группа';
+$lang['Where?'] = 'Где?';
+$lang['The whole gallery'] = 'Во всей галерее';
+$lang['ability to create sub-albums'] = 'позволить создавать вложенные альбомы';
+$lang['Which level of trust?'] = 'Каков уровень доверия?';
+$lang['sub-albums creation'] = 'создание вложенных альбомов';
+$lang['Upload Permissions'] = 'Разрешение на загрузку';
+$lang['Pending Photos'] = 'Фото на модерации';
+$lang['Permission added'] = 'Разрешение добавлено';
+$lang['Permission removed'] = 'Разрешение удалено';
+$lang['%s (the user)'] = '%s (пользователь)';
+$lang['%s (the group)'] = '%s (группа)';
+$lang['low trust'] = 'низкий уровень доверия';
+$lang['uploaded photos must be validated by an administrator'] = 'загруженные фотографии должны пройти проверку администратора';
+$lang['high trust'] = 'высокий уровень доверия';
+$lang['uploaded photos are directly displayed in the gallery'] = 'загруженные фотографии появляются в галерее сразу';
+$lang['%d photos validated'] = '%d фотографий проверено ';
+$lang['%d photos rejected'] = '%d фотографий отклонено';
+$lang['web size'] = 'размер для веба';
+$lang['Zoom'] = 'увеличение';
+$lang['Upload your own photos'] = 'загрузить свои собственные фотографии';
+$lang['%d photos uploaded by %s'] = '%d фотографий загружено %s';
+$lang['Validation page: %s'] = 'страница валидации: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d фотографий загружено в альбом "%s"';
+$lang['Hi administrators,'] = 'Привет, администратор,';
+
+$lang['No album, this photo is orphan'] = 'Без альбома, это фото одиноко(';
+
+$lang['Cancel'] = 'Отказаться';
+$lang['Permission updated'] = 'разрешение изменено';
+$lang['Delete permission'] = 'удалить разрешение';
+$lang['%u pending photos'] = '%u фотографий для проверки';
+$lang['Level 16'] = 'проверка';
+
+$lang['Edit a permission'] = 'создать разрешение';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Вы загрузили фото, администратор скоро их проверит, так как письмо о ваших фотографиях ему уже выслано';
+?>
