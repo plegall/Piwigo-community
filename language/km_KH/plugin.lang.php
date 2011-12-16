bc7a2d12b7a6546c84b8a3fcc6a8636152cc1fb0
ddtddt <ddtddt@piwigo.org>
2011-12-16 23:31:57 +0000

    [extensions] - community - add km_KH – ​ភាសា​ខ្មែរ​- khmer (Cambodia) Thanks to Vanndy
    git-svn-id: http://piwigo.org/svn/extensions/community@12753 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/km_KH/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'បន្ថែម​​សិទ្ធ​មួយ​';
+$lang['Who?'] = 'នណា?';
+$lang['any visitor'] = 'អ្នក​​ចូល​មើល​ផ្សេង​ៗ​';
+$lang['any registered user'] = 'អ្នក​ដែល​បាន​ចុះឈ្មោះ​ប្រើ​ប្រាស់​ផ្សេង​ទៀត​';
+$lang['a specific user'] = 'អ្នក​ប្រើ​ប្រាស់​ជាក់​លាក់​ម្នាក់​';
+$lang['a group'] = 'ក្រុម​មួយ​';
+$lang['Where?'] = 'ឯ​ណា?';
+$lang['The whole gallery'] = 'សៀវ​ភៅ​រូប​ថត​ទាំង​មូល​';
+$lang['ability to create sub-albums'] = 'សមត្ថ​ភាព​ដើម្បី​បង្កើត​សៀវ​ភៅ​រូប​ថត​រង​';
+$lang['Which level of trust?'] = 'ការ​ទុក​ចិត្ត​កំរិត​ណា?';
+$lang['sub-albums creation'] = 'ការ​បង្កើត​សៀវ​ភៅ​រូប​ថត​រង​';
+$lang['Upload Permissions'] = 'ការ​អនុញ្ញាត​ផ្ទុក​ឡើង​';
+$lang['Pending Photos'] = 'រូប​ថត​កំពុង​រង​ចាំ​ការ​អនុញ្ញាត​';
+$lang['Permission added'] = 'បាន​បន្ថែម​ការ​អនុញ្ញាត​';
+$lang['Permission removed'] = 'បាន​ដក​ការ​អនុញ្ញាត​ចេញ​';
+$lang['%s (the user)'] = '%s (នាក់​)';
+$lang['%s (the group)'] = '%s (ក្រុម​)';
+$lang['low trust'] = 'ការ​ទុក​ចិត្ត​កំរិត​ទាប​';
+$lang['uploaded photos must be validated by an administrator'] = 'រូប​ថត​ដែល​បាន​បញ្ចូល​ ត្រូវ​តែ​មានការ​ពិនិត្យ​ពី​អ្នក​គ្រប់​គ្រង';
+$lang['high trust'] = 'ការ​ទុក​ចិត្ត​កំរិត​ខ្ពស់​';
+$lang['uploaded photos are directly displayed in the gallery'] = 'រូប​ថត​ដែល​បាន​បន្ថែម​បាន​ភ្ជាប់​បង្ហាញ​ផ្ទាល់​ក្នុង​វិចិត្រ​សាល​';
+$lang['%d photos validated'] = '%d រូប​ថត​បាន​ពិនិត្យ​';
+$lang['%d photos rejected'] = '%d រូប​ថត​បាន​បោះ​បង់​';
+$lang['web size'] = 'ទំហំ​បណ្តាញ​';
+$lang['Zoom'] = 'ពង្រីក​';
+$lang['Upload your own photos'] = 'បញ្ចូល​រូប​ថត​របស់​អ្នក​';
+$lang['%d photos uploaded by %s'] = '%d រូប​ថត​ បាន​បញ្ចូល​ដោយ​ %s';
+$lang['Validation page: %s'] = 'ពិនិត្យ​ទំព័រ: %s';
+$lang['%d photos uploaded into album "%s"'] = '%d រូប​ថត​ បាន​បញ្ចូល​ក្នុង​សៀវភៅ​រូប​ថត​ឈ្មោះ​​ "%s"';
+$lang['Hi administrators,'] = 'សួរ​ស្តី​ អ្នក​គ្រប់​គ្រង,';
+
+$lang['No album, this photo is orphan'] = 'គ្មាន​សៀវភៅ​រូប​ថត, រូប​ថត​នេះ​គ្មាន​សណ្តាប់​ធ្នាប់​';
+
+$lang['Cancel'] = 'បោះ​បង់​';
+$lang['Permission updated'] = 'ការ​អនុញ្ញាត​បាន​កែ​ប្រែ​';
+$lang['Delete permission'] = 'លុប​ការ​អនុញ្ញាត​';
+$lang['%u pending photos'] = '%u រូប​ថត​កំពុង​រង​ចាំ';
+$lang['Level 16'] = 'កំពុង​រង​ចាំ​';
+
+$lang['Edit a permission'] = 'កែ​ប្រែ​ការ​អនុញ្ញាត​';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'រូប​ថត​របស់​អ្នក​កំពុង​រង​ចាំ​ការ​​ត្រួត​ពិនិត្យ​, អ្នកគ្រប់​គ្រង​នឹង​ផ្តល់​ដំណឺង​ដល់​អ្នក​ពេល​រួច​រាល់';
+
+$lang['Set Photo Properties'] = 'សំណុំ​លក្ខណះ​នៃ​រូប​ភាព';
+?>
