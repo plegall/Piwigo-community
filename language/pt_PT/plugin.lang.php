d319a846eac6bc6d36f23a68b4d666c2da59d599
ddtddt <ddtddt@piwigo.org>
2011-12-22 09:53:14 +0000

    [extensions] - community - add pt_PT - Português - Portuguese - Thanks to marcelomac
    git-svn-id: http://piwigo.org/svn/extensions/community@12781 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,70 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Adicionar uma permissão';
+$lang['Who?'] = 'Quem?';
+$lang['any visitor'] = 'Qualquer visitante';
+$lang['any registered user'] = 'Qualquer usuário registrado';
+$lang['a specific user'] = 'Um usuário específico';
+$lang['a group'] = 'Um grupo';
+$lang['Where?'] = 'Onde';
+$lang['The whole gallery'] = 'Toda a galeria';
+$lang['ability to create sub-albums'] = 'Capacidade para criar sub-álbuns';
+$lang['Which level of trust?'] = 'Qual nível de confiança?';
+$lang['sub-albums creation'] = 'Criação de sub-álbuns';
+$lang['Upload Permissions'] = 'Permissões de envio';
+$lang['Pending Photos'] = 'Fotos pendentes';
+$lang['Permission added'] = 'Permissão inserida';
+$lang['Permission removed'] = 'Permissão removida';
+$lang['%s (the user)'] =  '%s (o usuário)';
+$lang['%s (the group)'] = '%s (o grupo)';
+$lang['low trust'] = 'Baixa confiança';
+
+$lang['uploaded photos must be validated by an administrator'] = 'Fotos enviadas devem ser autorizadas por um administrador';
+
+$lang['high trust'] = 'baixa confiança';
+$lang['uploaded photos are directly displayed in the gallery'] = 'fotos enviadas são exibidas diretamente na galeria';
+
+$lang['%d photos validated'] = '%d fotos validadas';
+$lang['%d photos rejected'] =  '%d fotos rejeitadas';
+$lang['web size'] = 'tamanho web';
+$lang['Zoom'] = 'Zoom';
+$lang['Upload your own photos'] = 'Envie suas próprias fotos';
+$lang['%d photos uploaded by %s'] =  '%d fotos enviadas por %s';
+$lang['Validation page: %s'] = 'Página de validação: %s';
+$lang['%d photos uploaded into album "%s"'] =  '%d fotos enviadas no álbum "%s"';
+$lang['Hi administrators,'] = 'Olá administradores,';
+
+$lang['No album, this photo is orphan'] = 'Sem álbum, ésta foto é orfã';
+
+$lang['Cancel'] = 'Cancelar';
+$lang['Permission updated'] = 'Permissão atualizada';
+$lang['Delete permission'] = 'Permissão excluida';
+$lang['%u pending photos'] = '%u fotos pendentes';
+$lang['Level 16'] = 'Pendente';
+
+$lang['Edit a permission'] = 'Editar uma permissão';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Suas fotos estão esperando validação, os administradores foram notificados';
+
+$lang['Set Photo Properties'] = 'Definir as propriedades da foto';
+?>
