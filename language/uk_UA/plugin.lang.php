09beae02c8c5c604a5f89cbc5257a9ff361d6655
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-16 00:44:03 +0000

    [Community] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/community@13174 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos rejected'] = '%d фотографій відхилено';
+$lang['%d photos uploaded by %s'] = '%d фотографій, завантажених %s';
+$lang['%d photos uploaded into album "%s"'] = '%d фотографій завантажених в альбом "%s"';
+$lang['%d photos validated'] = '%d фотографій перевірено';
+$lang['%s (the group)'] = '%s (Група)';
+$lang['%s (the user)'] = '%s(користувач)';
+$lang['%u pending photos'] = '%u в очікування фотографій';
+$lang['a group'] = 'група';
+$lang['a specific user'] = 'конкретного користувача';
+$lang['ability to create sub-albums'] = 'можливість створювати вкладені альбоми';
+$lang['Add a permission'] = 'Додати дозвіл';
+$lang['any registered user'] = 'будь-який зареєстрований користувач';
+$lang['any visitor'] = 'будь-який відвідувач';
+$lang['Cancel'] = 'Скасувати';
+$lang['Delete permission'] = 'Вилучення дозволу';
+$lang['Edit a permission'] = 'Зміна дозволу';
+$lang['Hi administrators,'] = 'Привіт адміністратори,';
+$lang['high trust'] = 'висока довіра';
+$lang['Level 16'] = 'В очікуванні';
+$lang['low trust'] = 'низький рівень довіри';
+$lang['No album, this photo is orphan'] = 'Немає альбомів, ця фотографія сирота';
+$lang['Pending Photos'] = 'В очікуванні фотографії';
+$lang['Permission added'] = 'Дозвіл додано';
+$lang['Permission removed'] = 'Дозвіл видалено';
+$lang['Permission updated'] = 'Дозвіл оновлено';
+$lang['Set Photo Properties'] = 'Встановити властивості фотографії';
+$lang['sub-albums creation'] = 'створення вкладених альбомів';
+$lang['The whole gallery'] = 'Вся галерея';
+$lang['Upload Permissions'] = 'Дозвол завантажувати';
+$lang['Upload your own photos'] = 'Завантажити свої фотографії';
+$lang['uploaded photos are directly displayed in the gallery'] = 'завантажені фотографії безпосередньо відображаються в галереї';
+$lang['uploaded photos must be validated by an administrator'] = 'завантажені фотографії повиненні бути перевірені адміністратором';
+$lang['Validation page: %s'] = 'Перевірено сторінки: %s';
+$lang['web size'] = 'веб-розмір';
+$lang['Where?'] = 'Де?';
+$lang['Which level of trust?'] = 'Який рівень довіри?';
+$lang['Who?'] = 'Хто?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Ваші фотографії чекають перевірки, адміністратори були повідомлені';
+$lang['Zoom'] = 'Збільшити';
+?>
