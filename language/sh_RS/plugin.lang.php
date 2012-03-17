8df8de0b7a8a9b627bcd22eae5712d86399454a5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-17 08:41:17 +0000

    [Community] Add sh_RS, thanks to : vladamilanovic
    git-svn-id: http://piwigo.org/svn/extensions/community@13581 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sh_RS/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos rejected'] = '%d fotografija odbijeno';
+$lang['%d photos uploaded by %s'] = '%d fotografija poslato od strane %s';
+$lang['%d photos uploaded into album "%s"'] = '%d fotografije poslato u album "%s"';
+$lang['%d photos validated'] = '%d fotografija odobreno';
+$lang['%s (the group)'] = '%s (grupa)';
+$lang['%s (the user)'] = '%s (korisnik)';
+$lang['%u pending photos'] = '%u fotografija na čekanju';
+$lang['a group'] = 'grupa';
+$lang['a specific user'] = 'određeni korisnik';
+$lang['ability to create sub-albums'] = 'sposobnost da se kreiraju pod-albumi';
+$lang['Add a permission'] = 'Dodaj dozvolu';
+$lang['any registered user'] = 'bilo koji registrovani korisnik';
+$lang['any visitor'] = 'bilo koji posetilac';
+$lang['Cancel'] = 'Otkaži';
+$lang['Delete permission'] = 'Obriši dozvolu';
+$lang['Edit a permission'] = 'Uredi dozvole';
+$lang['Hi administrators,'] = 'Pozdrav administratori,';
+$lang['high trust'] = 'visoko poverenje';
+$lang['Level 16'] = 'Na čekanje';
+$lang['low trust'] = 'nisko poverenje';
+$lang['No album, this photo is orphan'] = 'Nema albuma, ova fotografija je napuštena';
+$lang['Pending Photos'] = 'Fotografije na čekanju';
+$lang['Permission added'] = 'Dozvola dodata';
+$lang['Permission removed'] = 'Dozvola uklonjena';
+$lang['Permission updated'] = 'Dozvola izmenjena';
+$lang['Set Photo Properties'] = 'Postavi svojstva fotografije';
+$lang['sub-albums creation'] = 'kreiranje pod-albuma';
+$lang['The whole gallery'] = 'Cela galerija';
+$lang['Upload Permissions'] = 'Dozvole za slanje';
+$lang['Upload your own photos'] = 'Pošalji svoje fotografije';
+$lang['uploaded photos are directly displayed in the gallery'] = 'poslate fotografije su odmah prikazane u galeriji';
+$lang['uploaded photos must be validated by an administrator'] = 'poslate fotografije moraju biti odobrene od strane administratora';
+$lang['Validation page: %s'] = 'Strana za odobravanje';
+$lang['web size'] = 'veličina za web';
+$lang['Where?'] = 'Gde?';
+$lang['Which level of trust?'] = 'Koji je nivo poverenja?';
+$lang['Who?'] = 'Ko?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Vaše fotografije čekaju odobrenje, administratori su obavešteni';
+$lang['Zoom'] = 'Uvećaj';
+?>
