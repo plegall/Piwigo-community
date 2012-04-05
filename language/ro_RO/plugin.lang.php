aa16777c083ac1562a2f71a8fff88a1c8628ff30
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-05 18:34:29 +0000

    [Community] Add ro_RO, thanks to : madalinul1
    git-svn-id: http://piwigo.org/svn/extensions/community@13925 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos rejected'] = '%d fotografii respinse';
+$lang['%d photos uploaded by %s'] = '%d fotografii incarcate de %s';
+$lang['%d photos uploaded into album "%s"'] = '%d fotografii incarcate in albumul "%s"';
+$lang['%d photos validated'] = '%d fotografii validate';
+$lang['%s (the group)'] = '%s (grupul)';
+$lang['%s (the user)'] = '%s (userul)';
+$lang['%u pending photos'] = '%u fotografii in asteptare';
+$lang['a group'] = 'un grup';
+$lang['a specific user'] = 'un user specificat';
+$lang['ability to create sub-albums'] = 'abilitatea de a crea sub-albumuri';
+$lang['Add a permission'] = 'Adauga o permisiune';
+$lang['any registered user'] = 'orice user inregistrat';
+$lang['any visitor'] = 'orice vizitator';
+$lang['Cancel'] = 'Anulare';
+$lang['Delete permission'] = 'Permisiuni de stergere';
+$lang['Edit a permission'] = 'Editeaza o permisiune';
+$lang['Hi administrators,'] = 'Buna administratorilor :)';
+$lang['high trust'] = 'incredere ridicata';
+$lang['Level 16'] = 'In asteptare';
+$lang['low trust'] = 'incredere joasa';
+$lang['No album, this photo is orphan'] = 'Fara album, poza asta e orfana :(';
+$lang['Pending Photos'] = 'Fotografii in asteptare';
+$lang['Permission added'] = 'Permisiuni adaugate';
+$lang['Permission removed'] = 'Permisiuni revocate';
+$lang['Permission updated'] = 'Permisiuni actualizate';
+$lang['Set Photo Properties'] = 'Seteaza proprietatile fotografiei';
+$lang['sub-albums creation'] = 'creare de sub-albume';
+$lang['The whole gallery'] = 'Toata galeria';
+$lang['Upload Permissions'] = 'Permisiuni de upload';
+$lang['Upload your own photos'] = 'Incarca propriile fotografii';
+$lang['uploaded photos are directly displayed in the gallery'] = 'Fotografiile incarcate sunt afisate direct in galerie';
+$lang['uploaded photos must be validated by an administrator'] = 'pozele incarcate trebuie sa fie validate de un administrator';
+$lang['Validation page: %s'] = 'Pagina de validare %s';
+$lang['web size'] = 'marimea web';
+$lang['Where?'] = 'Unde?';
+$lang['Which level of trust?'] = 'Ce nivel de incredere?';
+$lang['Who?'] = 'Cine?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Fotografiile tale asteapta validarea, administratorii au fost notificati';
+$lang['Zoom'] = 'Zoom';
+?>
