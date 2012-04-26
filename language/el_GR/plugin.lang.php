faa00397419fa813492202b6f1c48f00ec8068e6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-26 21:44:26 +0000

    [community] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/community@14455 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s (the group)'] = '%s (η ομάδα)';
+$lang['%s (the user)'] = '%s (ο χρήστης)';
+$lang['Add a permission'] = 'Προσθέστε μια άδεια';
+$lang['Pending Photos'] = 'Φωτογραφίες Εν αναμονή';
+$lang['Permission added'] = 'Άδεια προστέθηκε';
+$lang['Permission removed'] = 'Άδεια αφαιρέθηκε';
+$lang['The whole gallery'] = 'Η όλη γκαλερί';
+$lang['Upload Permissions'] = 'Ανεβάστε Δικαιώματα';
+$lang['Where?'] = 'Που;';
+$lang['Which level of trust?'] = 'Ποιο επίπεδο της εμπιστοσύνης;';
+$lang['Who?'] = 'Ποιός;';
+$lang['a group'] = 'μια ομάδα';
+$lang['a specific user'] = 'ένας συγκεκριμένος χρήστης';
+$lang['ability to create sub-albums'] = 'δυνατότητα να δημιουργήσετε υπο-λεύκωμα';
+$lang['any registered user'] = 'κάθε εγγεγραμμένος χρήστης';
+$lang['any visitor'] = 'κάθε επισκέπτης';
+$lang['high trust'] = 'υψηλής εμπιστοσύνης';
+$lang['low trust'] = 'χαμηλής εμπιστοσύνης';
+$lang['sub-albums creation'] = 'δημιουργία υπο-λευλώματος';
+$lang['uploaded photos must be validated by an administrator'] = 'Οι φωτογραφίες που ανέβηκαν θα πρέπει να επικυρωθούν από διαχειριστή';
+?>
