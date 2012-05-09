4f8fd75364f77a29b6e0285c6c012c6730a39fb9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-09 04:48:50 +0000

    [community] Update da_DK, thanks to : Egorip
    git-svn-id: http://piwigo.org/svn/extensions/community@14874 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos rejected'] = '%d billeder afvist';
+$lang['%d photos uploaded by %s'] = '%d billeder uploaded af %s';
+$lang['%d photos uploaded into album "%s"'] = '%d billeder uploadet til album "%s"';
+$lang['%d photos validated'] = '%d validerede fotos';
+$lang['%s (the group)'] = '%s (gruppen)';
+$lang['%s (the user)'] = '%s (bruger)';
+$lang['%u pending photos'] = '%u ventende fotos';
+$lang['Add a permission'] = 'Tilføj en tilladelse';
+$lang['Cancel'] = 'Annuller';
+$lang['Delete permission'] = 'Slet tilladelse';
+$lang['Edit a permission'] = 'Rediger tilladelse';
+$lang['Hi administrators,'] = 'Hej administratorer,';
+$lang['Level 16'] = 'ventende ';
+$lang['No album, this photo is orphan'] = 'Intet album, dette billede er forældreløs';
+$lang['Pending Photos'] = 'Ventende Billeder';
+$lang['Permission added'] = 'Tilladelse tilføjet';
+$lang['Permission removed'] = 'Tilladelse fjernet';
+$lang['Permission updated'] = 'Tilladelse opdateret';
+$lang['Set Photo Properties'] = 'Sæt Foto egenskaber';
+$lang['The whole gallery'] = 'Hele galleriet';
+$lang['Upload Permissions'] = 'Upload Tilladelser';
+$lang['Upload your own photos'] = 'Upload dine egne billeder';
+$lang['Validation page: %s'] = 'Validering side: %s';
+$lang['Where?'] = 'Hvor?';
+$lang['Which level of trust?'] = 'Hvilket niveau af tillid?';
+$lang['Who?'] = 'Hvem?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Dine billeder venter på validering, administratorer er blevet oplyst om dette';
+$lang['Zoom'] = 'Zoom';
+$lang['a group'] = 'en gruppe';
+$lang['a specific user'] = 'en bestemt bruger';
+$lang['ability to create sub-albums'] = 'evne til at skabe under-albummer';
+$lang['any registered user'] = 'enhver registreret bruger';
+$lang['any visitor'] = 'alle besøgende';
+$lang['high trust'] = 'høj tillid';
+$lang['low trust'] = 'lav tillid';
+$lang['sub-albums creation'] = 'under-albummer oprettelse';
+$lang['uploaded photos are directly displayed in the gallery'] = 'uploadede billeder direkte vises i galleriet';
+$lang['uploaded photos must be validated by an administrator'] = 'uploadede billeder skal godkendes af en administrator';
+$lang['web size'] = 'web-størrelse';
+?>
