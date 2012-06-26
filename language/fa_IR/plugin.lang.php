34758764084ae23d0d6c48e081b59ecc20a5d15c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-26 04:58:03 +0000

    [community] Update fa_IR, thanks to : Garcia
    git-svn-id: http://piwigo.org/svn/extensions/community@16038 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos rejected'] = '$d عکس پذیرفته نشدند';
+$lang['%d photos uploaded by %s'] = '%d عکس بوسیله ی %s آپلود شدند';
+$lang['%d photos uploaded into album "%s"'] = '%d عکس در آلبوم "%s" آپلود شدند';
+$lang['%d photos validated'] = '%d عکس پذیرفته شدند';
+$lang['%s (the group)'] = '%s (گروه)';
+$lang['%s (the user)'] = '%s (کاربر)';
+$lang['%u pending photos'] = '%u عکس در انتظار پذیرفته شدن';
+$lang['Add a permission'] = 'اضافه کردن یک دسترسی';
+$lang['Cancel'] = 'لغو';
+$lang['Delete permission'] = 'پاک کردن دسترسی';
+$lang['Edit a permission'] = 'ویرایش یک دسترسی';
+$lang['Hi administrators,'] = 'درود مدیران،';
+$lang['Level 16'] = 'در انتظار پذیرش ';
+$lang['No album, this photo is orphan'] = 'بدون البوم، این عکس بی ریشه می باشد';
+$lang['Pending Photos'] = 'عکس های در انتظار پذیرش';
+$lang['Permission added'] = 'دسترسی افزوده شد';
+$lang['Permission removed'] = 'دسترسی پاک شد';
+$lang['Permission updated'] = 'دسترسی بروز شد';
+$lang['Set Photo Properties'] = 'تنظیم ویژگی های عکس';
+$lang['The whole gallery'] = 'تمام گالری';
+$lang['Upload Permissions'] = 'آپلود دسترسی ها';
+$lang['Upload your own photos'] = 'آپلود عکس های خودتان';
+$lang['Validation page: %s'] = 'صفحه ی پذیرش: %s';
+$lang['Where?'] = 'کجا؟';
+$lang['Which level of trust?'] = 'چه مرحله از اعتماد؟';
+$lang['Who?'] = 'چه کسی؟';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'عکس های شما در انتظار پذیرش می باشند، مدیران از عکس ها آگاه خواهند شد';
+$lang['Zoom'] = 'بزرگنمایی';
+$lang['a group'] = 'یک گروه';
+$lang['a specific user'] = 'یک کاربر ویژه';
+$lang['ability to create sub-albums'] = 'امکان ساختن زیر آلبوم';
+$lang['any registered user'] = 'هر کاربر ثبت نام شده ای';
+$lang['any visitor'] = 'هر بازدید کننده ای';
+$lang['high trust'] = 'اعتماد بالا';
+$lang['low trust'] = 'اعتماد پایین';
+$lang['sub-albums creation'] = 'ساخت زیر آلبوم';
+$lang['uploaded photos are directly displayed in the gallery'] = 'عکس های آپلود شده بطور مستقیم در گالری نمایش داده می شوند';
+$lang['uploaded photos must be validated by an administrator'] = 'عکس های آپلود شده باید بوسیله ی یک مدیر پذیرفته شوند';
+$lang['web size'] = 'وب سایز (Websize)';
+?>
