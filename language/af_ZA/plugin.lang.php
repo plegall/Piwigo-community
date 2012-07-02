1cbf5963628f7473306d6cb8a9ed2fe5e6084c2c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-02 00:03:40 +0000

    [community] Update af_ZA, thanks to : jpmeijers
    git-svn-id: http://piwigo.org/svn/extensions/community@16249 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/af_ZA/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos rejected'] = '%d fotos afgekeur';
+$lang['%d photos uploaded by %s'] = '%d fotos opgelaai deur %s';
+$lang['%d photos uploaded into album "%s"'] = '%d fotos opgelaai na album "%s"';
+$lang['%d photos validated'] = '%d fotos nagegaan';
+$lang['%s (the group)'] = '%s (die groep)';
+$lang['%s (the user)'] = '%s (die gebruiker)';
+$lang['%u pending photos'] = '%u onafgehandelde fotos';
+$lang['Add a permission'] = 'Voeg toegangsregte by';
+$lang['Cancel'] = 'Kanselleer';
+$lang['Delete permission'] = 'Verwyder toegangsregte';
+$lang['Edit a permission'] = 'Verabder toegangsregte';
+$lang['Hi administrators,'] = 'Hi administreerders,';
+$lang['Level 16'] = 'Onafgehandeld';
+$lang['No album, this photo is orphan'] = 'Geen album, hierdie is \'n weesfoto';
+$lang['Pending Photos'] = 'Onafgehandelde fotos';
+$lang['Permission added'] = 'Toegangsregte toegevoeg';
+$lang['Permission removed'] = 'Toegangsregte verwyder';
+$lang['Permission updated'] = 'Toegangsregte aangepas';
+$lang['Set Photo Properties'] = 'Verander foto se kenmerke';
+$lang['The whole gallery'] = 'Die hele gallery';
+$lang['Upload Permissions'] = 'Oplaai toegangsregte';
+$lang['Upload your own photos'] = 'Laai jou eie fotos op';
+$lang['Validation page: %s'] = 'Nagaan bladsy: %s';
+$lang['Where?'] = 'Waar?';
+$lang['Which level of trust?'] = 'Watter vlak van vertroue?';
+$lang['Who?'] = 'Wie?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Jou fotos is gereed vir nagaan. Administreerders is in kennis gestel.';
+$lang['Zoom'] = 'Zoom';
+$lang['a group'] = '\'n groep';
+$lang['a specific user'] = '\'n spesifieke gebruiker';
+$lang['ability to create sub-albums'] = 'kan sub-albums maak';
+$lang['any registered user'] = 'enige geregistreerde gebruiker';
+$lang['any visitor'] = 'enige gas';
+$lang['high trust'] = 'hoë vlak van vertroue';
+$lang['low trust'] = 'lae vlak van vertroue';
+$lang['sub-albums creation'] = 'maak van sub-albums';
+$lang['uploaded photos are directly displayed in the gallery'] = 'opgelaaide fotos word dadelik in die gallery vertoon';
+$lang['uploaded photos must be validated by an administrator'] = 'opgelaaide fotos moet eers deur \'n administreerder goedgekeur word';
+$lang['web size'] = 'web grootte';
+?>
