df639f9a7a00772599fa2f18d1e6451a148c6c5c
ddtddt <ddtddt@piwigo.org>
2012-07-15 11:32:55 +0000

    [extensions] - community[trunk] - add fr_CA - Quebec - Québécois - Thanks to TiOger
    git-svn-id: http://piwigo.org/svn/extensions/community@16746 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based picture gallery                                  |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2011 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+$lang['Add a permission'] = 'Ajouter une permission';
+$lang['Who?'] = 'Qui ?';
+$lang['any visitor'] = 'n\'importe quel visiteur';
+$lang['any registered user'] = 'n\'importe quel utilisateur enregistré';
+$lang['a specific user'] = 'un utilisateur en particulier';
+$lang['a group'] = 'un groupe';
+$lang['Where?'] = 'Où ?';
+$lang['The whole gallery'] = 'La galerie toute entière';
+$lang['ability to create sub-albums'] = 'possibilité de créer des sous-albums';
+$lang['Which level of trust?'] = 'Quel degré de confiance ?';
+$lang['sub-albums creation'] = 'création de sous-albums';
+$lang['Upload Permissions'] = 'Permissions d\'ajout';
+$lang['Pending Photos'] = 'Photos en attente';
+$lang['Permission added'] = 'Permission enregistrée';
+$lang['Permission removed'] = 'Permission supprimée';
+$lang['%s (the user)'] = '%s (l\'utilisateur)';
+$lang['%s (the group)'] = '%s (le groupe)';
+$lang['low trust'] = 'confiance faible';
+$lang['uploaded photos must be validated by an administrator'] = 'les photos ajoutées doivent être validées par un administrateur';
+$lang['high trust'] = 'confiance élevée';
+$lang['uploaded photos are directly displayed in the gallery'] = 'les photos ajoutées sont directement visibles dans la galerie';
+$lang['%d photos validated'] = '%d photos validées';
+$lang['%d photos rejected'] = '%d photos rejetées';
+$lang['web size'] = 'taille web';
+$lang['Zoom'] = 'Zoom';
+$lang['Upload your own photos'] = 'Ajoutez vos propres photos';
+$lang['%d photos uploaded by %s'] = '%d photos ajoutées par %s';
+$lang['Validation page: %s'] = 'Page de validation : %s';
+$lang['%d photos uploaded into album "%s"'] = '%d photos ajoutée à l\'album "%s"';
+$lang['Hi administrators,'] = 'Bonjours, chers administrateurs,';
+
+$lang['No album, this photo is orphan'] = 'Aucun album, cette photo est orpheline';
+
+$lang['Cancel'] = 'Annuler';
+$lang['Permission updated'] = 'Permission mise à jour';
+$lang['Delete permission'] = 'Supprimer la permission';
+$lang['%u pending photos'] = '%u photos en attente';
+$lang['Level 16'] = 'En attente';
+
+$lang['Edit a permission'] = 'Éditer une permission';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Vos photos sont en attente de validation, les administrateurs sont prévenus';
+
+$lang['Set Photo Properties'] = 'Définir les propriétés de la photo'; 
+?>
