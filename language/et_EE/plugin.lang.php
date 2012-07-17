ff2ed22a7f233489ffe805ba67284497fd2860d0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-17 19:55:50 +0000

    [community] Update et_EE, thanks to : kristian
    git-svn-id: http://piwigo.org/svn/extensions/community@16809 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos rejected'] = '%d fotot lükati tagasi';
+$lang['%d photos uploaded by %s'] = '%d fotot laadis üles kasutaja %s';
+$lang['%d photos uploaded into album "%s"'] = '%d fotot laeti üles albumisse "%s"';
+$lang['%d photos validated'] = '%d fotot kinnitatud';
+$lang['%s (the group)'] = '%s (grupp)';
+$lang['%s (the user)'] = '%s (kasutaja)';
+$lang['%u pending photos'] = '%u ootel fotot';
+$lang['Add a permission'] = 'Lisa õigusi';
+$lang['Cancel'] = 'Katkesta';
+$lang['Delete permission'] = 'Kustutamisõigus';
+$lang['Edit a permission'] = 'Muuda õigusi';
+$lang['Hi administrators,'] = 'Tere, administraatorid';
+$lang['Level 16'] = 'Ootel';
+$lang['No album, this photo is orphan'] = 'Puudub album, see foto on üksinda jäetud';
+$lang['Pending Photos'] = 'Ootel fotosid';
+$lang['Permission added'] = 'Õigus antud';
+$lang['Permission removed'] = 'Õigus kõrvaldatud';
+$lang['Permission updated'] = 'Õigused uuendatud';
+$lang['Set Photo Properties'] = 'Määra foto omadused';
+$lang['The whole gallery'] = 'Terve galerii';
+$lang['Upload Permissions'] = 'Üleslaadimise õigus';
+$lang['Upload your own photos'] = 'Laadi oma fotod ülesse';
+$lang['Validation page: %s'] = 'Kinnitamisleht: %s';
+$lang['Where?'] = 'Kuhu?';
+$lang['Which level of trust?'] = 'Mis usaldustasemega?';
+$lang['Who?'] = 'Kes?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Su fotod ootavad kinnitust, administraatorid on teavitatud';
+$lang['Zoom'] = 'Suurendus';
+$lang['a group'] = 'üks grupp';
+$lang['a specific user'] = 'üks konkreetne kasutaja';
+$lang['ability to create sub-albums'] = 'õigus tekitada alam-albumeid';
+$lang['any registered user'] = 'kestahes registreeritud kasutaja';
+$lang['any visitor'] = 'kestahes külaline';
+$lang['high trust'] = 'kõrge usaldus';
+$lang['low trust'] = 'madal usaldus';
+$lang['sub-albums creation'] = 'alam-albumid tekitatud';
+$lang['uploaded photos are directly displayed in the gallery'] = 'üles laaditud fotod on kohe kuvatud galeriis';
+$lang['uploaded photos must be validated by an administrator'] = 'üles laaditud fotod peavad kinnituse saama administraatorilt';
+$lang['web size'] = 'veebi-suurus';
+?>
