f1e4f6622165a5ee0efffc67d440f42609dabbe0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-23 17:39:20 +0000

    [community] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/community@16941 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos rejected'] = '%d 已拒绝的图片';
+$lang['%d photos uploaded by %s'] = '%d 图片上传者 %s';
+$lang['%d photos uploaded into album "%s"'] = '%d 已上传的图片 "%s"';
+$lang['%d photos validated'] = '%d 已批准的图片';
+$lang['%s (the group)'] = '%s (组)';
+$lang['%s (the user)'] = '%s (用户)';
+$lang['%u pending photos'] = '%u 待审核的图片';
+$lang['Add a permission'] = '添加一项授权';
+$lang['Cancel'] = '取消';
+$lang['Delete permission'] = '删除此授权';
+$lang['Edit a permission'] = '编辑授权';
+$lang['Hi administrators,'] = '管理员 您好，';
+$lang['Level 16'] = '审核';
+$lang['No album, this photo is orphan'] = '没有相册，该图片是孤立的';
+$lang['Pending Photos'] = '待审核图片';
+$lang['Permission added'] = '该项授权已添加';
+$lang['Permission removed'] = '该项授权已删除';
+$lang['Permission updated'] = '该项授权已更新';
+$lang['Set Photo Properties'] = '设置图片属性';
+$lang['The whole gallery'] = '整个相册';
+$lang['Upload Permissions'] = '上传授权';
+$lang['Upload your own photos'] = '上传您的图片';
+$lang['Validation page: %s'] = '审核页: %s';
+$lang['Where?'] = '授权相册';
+$lang['Which level of trust?'] = '授权等级';
+$lang['Who?'] = '授权对象';
+$lang['Your photos are waiting for validation, administrators have been notified'] = '已通知管理员，您上传的图片正等待管理员审核。';
+$lang['Zoom'] = '放大';
+$lang['a group'] = '组';
+$lang['a specific user'] = '特定的用户';
+$lang['ability to create sub-albums'] = '允许创建子相册';
+$lang['any registered user'] = '任何注册用户';
+$lang['any visitor'] = '任何访问者';
+$lang['high trust'] = '高级';
+$lang['low trust'] = '低级';
+$lang['sub-albums creation'] = '创建子相册';
+$lang['uploaded photos are directly displayed in the gallery'] = '上传图片后直接显示于相册';
+$lang['uploaded photos must be validated by an administrator'] = '上传图片后须经管理员审核';
+$lang['web size'] = '网页大小';
+?>
