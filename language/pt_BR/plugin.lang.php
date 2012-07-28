a745f165f9339e84444af56f4a1d23389fd610a7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-28 08:00:45 +0000

    [community] Update pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/community@17183 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d photos rejected'] = '%d fotos rejeitadas';
+$lang['%d photos uploaded by %s'] = '%d fotos enviadas por %s';
+$lang['%d photos uploaded into album "%s"'] = '%d fotos enviadas para a categoria "%s"';
+$lang['%d photos validated'] = '%d fotos validadas';
+$lang['%s (the group)'] = '%s (o grupo)';
+$lang['%s (the user)'] = '%s (o usuário)';
+$lang['%u pending photos'] = '%u fotos pendentes';
+$lang['Add a permission'] = 'adicione uma permissão';
+$lang['Cancel'] = 'Cancelar';
+$lang['Delete permission'] = 'Apagar permissão';
+$lang['Edit a permission'] = 'Editar uma permissão';
+$lang['Hi administrators,'] = 'Olá Administradores,';
+$lang['Level 16'] = 'Pendente';
+$lang['No album, this photo is orphan'] = 'Sem categoria, essa foto é órfã';
+$lang['Pending Photos'] = 'Fotos pendentes';
+$lang['Permission added'] = 'Permissão adicionada';
+$lang['Permission removed'] = 'Permissão revogada';
+$lang['Permission updated'] = 'Permissão atualizada';
+$lang['Set Photo Properties'] = 'Defina as propriedades da foto';
+$lang['The whole gallery'] = 'Toda Galeria';
+$lang['Upload Permissions'] = 'Permissões de envio';
+$lang['Upload your own photos'] = 'Envie suas próprias fotos';
+$lang['Validation page: %s'] = 'Página de validação: %s';
+$lang['Where?'] = 'Onde?';
+$lang['Which level of trust?'] = 'Qual nível de confiança?';
+$lang['Who?'] = 'Quem?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Suas fotos estão aguardando  validação, os administradores foram notificados';
+$lang['Zoom'] = 'Zoom';
+$lang['a group'] = 'um grupo';
+$lang['a specific user'] = 'um usuário específico';
+$lang['ability to create sub-albums'] = 'habilidade para criar sub-categorias';
+$lang['any registered user'] = 'qualquer usuário registrado';
+$lang['any visitor'] = 'qualquer visitante';
+$lang['high trust'] = 'Confiança alta';
+$lang['low trust'] = 'Confiança baixa';
+$lang['sub-albums creation'] = 'Criação de sub-categorias';
+$lang['uploaded photos are directly displayed in the gallery'] = 'Fotos enviadas são  mostradas diretamente na galeria';
+$lang['uploaded photos must be validated by an administrator'] = 'Fotos enviadas devem ser validadas por um administrador';
+$lang['web size'] = 'tamanho para web';
+?>
