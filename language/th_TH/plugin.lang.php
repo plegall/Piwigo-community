d2b1b36a0da5de306ac48c1168d4f2b29d832fc2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-01 05:57:33 +0000

    [community] Update th_TH, thanks to :
    git-svn-id: http://piwigo.org/svn/extensions/community@17253 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/th_TH/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Upload Permissions'] = 'สิทธิ์ในการอัพโหลด';
+$lang['Upload your own photos'] = 'อัพโหลดรูปถ่ายของคุณเอง';
+$lang['Validation page: %s'] = 'หน้าการยืนยัน : %s';
+$lang['Where?'] = 'ที่ไหน?';
+$lang['Which level of trust?'] = 'มีระดับความน่าเชื่อถือ?';
+$lang['Who?'] = 'ใคร?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'ภาพถ่ายของคุณกำลังรอการตรวจสอบ, เมื่อผู้ผู้ดูแลระบบได้รับการแจ้ง.';
+$lang['Zoom'] = 'ขยาย';
+$lang['a group'] = 'เป็นกลุ่ม';
+$lang['a specific user'] = 'เฉพาะผู้ใช้ที่ระบุ';
+$lang['ability to create sub-albums'] = 'สามารถสร้างอัลบั้มย่อย';
+$lang['any registered user'] = 'ผู้ใช้งานที่ลงทะเบียน';
+$lang['any visitor'] = 'ผู้เข้าชม';
+$lang['high trust'] = 'ความน่าเชื่อถือสูง';
+$lang['low trust'] = 'ความน่าเชื่อถือต่ำ';
+$lang['sub-albums creation'] = 'การสร้างอัลบั้มย่อย';
+$lang['uploaded photos are directly displayed in the gallery'] = 'รูปภาพที่อัปโหลดจะถูกแสดงในแกลเลอรี่โดยตรง';
+$lang['uploaded photos must be validated by an administrator'] = 'รูปภาพที่อัปโหลดจะต้องถูกตรวจสอบโดยผู้ดูแลระบบ';
+$lang['web size'] = 'ขนาดสำหรับเว็บไซต์';
+$lang['%d photos rejected'] = '%d รูป ที่ไม่ได้รับการยืนยัน';
+$lang['%d photos uploaded by %s'] = '%d รูป ที่อัพโหลดโดย %s';
+$lang['%d photos uploaded into album "%s"'] = '%d รูป ที่ได้อัพโหลดไปยังอัลบั้ม "%s"';
+$lang['%d photos validated'] = '%d รูปที่ถูกยืนยัน';
+$lang['%s (the group)'] = '%s (กลุ่ม)';
+$lang['%s (the user)'] = '%s (ผู้ใช้งาน)';
+$lang['%u pending photos'] = '%u รูปถ่ายที่กำลังรอดำเนินการ';
+$lang['Add a permission'] = 'เพิ่มการอนุญาต';
+$lang['Delete permission'] = 'ลบการอนุญาต';
+$lang['Edit a permission'] = 'แก้ไขการอนุญาต';
+$lang['Hi administrators,'] = 'สวัสดีผู้ดูแลระบบ';
+$lang['Level 16'] = 'กำลังรอดำเนินการ';
+$lang['No album, this photo is orphan'] = 'ไม่มีอัลบั้ม, รูปภ่ายไม่่มีอัลบั้้ม';
+$lang['Pending Photos'] = 'รูปถ่ายที่กำลังรอดำเนินการ';
+$lang['Permission added'] = 'เพิ่มการอนุญาต';
+$lang['Permission removed'] = 'ลบการอนุญาตออก';
+$lang['Permission updated'] = 'อัพเดตการอนุญาต';
+$lang['Set Photo Properties'] = 'ตั้งค่าคุณสมบัติรูปถ่าย';
+$lang['The whole gallery'] = 'แกลเลอรี่ทั้งหมด';
+$lang['Cancel'] = 'ยกเลิก';
+?>
