a94b088e1fd8b291ebfe78d973ef16f9555c84db
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-21 07:58:47 +0000

    [community] Update he_IL, thanks to : nitzanmor
    git-svn-id: http://piwigo.org/svn/extensions/community@17609 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['The whole gallery'] = 'כל הגלריה';
+$lang['Where?'] = 'איפה?';
+$lang['Who?'] = 'מי?';
+$lang['Cancel'] = 'בטל';
+$lang['Delete permission'] = 'מחק הרשאות';
+$lang['Edit a permission'] = 'ערוך הרשאות';
+$lang['%d photos uploaded into album "%s"'] = '%d תמונות העלו לתוך אלבום %s';
+$lang['%d photos validated'] = '%d תמונות אושרו';
+$lang['%d photos uploaded by %s'] = '%d תמונות העלט ע"י %s';
+$lang['%d photos rejected'] = '%d תמונות נדחו';
+?>
