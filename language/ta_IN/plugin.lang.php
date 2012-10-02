2474d9428579f7f70e6466214cc3046209c7ccd4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-02 18:48:50 +0000

    [community] Update ta_IN, thanks to : sivastar
    git-svn-id: http://piwigo.org/svn/extensions/community@18434 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ta_IN/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Upload Permissions'] = 'அனுமதிகள் பதிவேற்று';
+$lang['Upload your own photos'] = 'உங்கள் சொந்த புகைப்படங்களை பதிவேற்றலாம்';
+$lang['Validation page: %s'] = 'சரிபார்த்தல் பக்கம்: %s';
+$lang['Where?'] = 'எங்கே?';
+$lang['Which level of trust?'] = 'எந்த நிலை நம்பிக்கை?';
+$lang['Who?'] = 'யார்?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'உங்கள் புகைப்படங்கள் மதிப்பாய்விற்காகக் காத்திருக்கிறது, நிர்வாகிகளுக்கு அறிவிக்கப்பட்டுள்ளது';
+$lang['Zoom'] = 'பெரிதாக்கம்';
+$lang['a group'] = 'ஒரு குழு';
+$lang['a specific user'] = 'ஒரு குறிப்பிட்ட பயனர்';
+$lang['ability to create sub-albums'] = 'துணை ஆல்பங்களை உருவாக்க திறன்';
+$lang['any registered user'] = 'ஏதேனும் பதிவுபெற்ற பயனர்';
+$lang['any visitor'] = 'ஏதேனும் பார்வையாளர்';
+$lang['high trust'] = 'அதிக நம்பிக்கை';
+$lang['low trust'] = 'குறைந்த நம்பிக்கை';
+$lang['sub-albums creation'] = 'துணை ஆல்பங்கள் உருவாக்கம்';
+$lang['uploaded photos are directly displayed in the gallery'] = 'பதிவேற்றப்பட்ட புகைப்படங்கள் நேரடியாகக் காட்சியகத்தில் காட்டப்படும்';
+$lang['uploaded photos must be validated by an administrator'] = 'பதிவேற்றப்பட்ட புகைப்படங்கள் நிர்வாகியால் சரிபார்க்க வேண்டும்';
+$lang['web size'] = 'தள அளவு';
+$lang['%d photos rejected'] = '%d புகைப்படங்கள் நிராகரிக்கப்பட்டது';
+$lang['%d photos uploaded by %s'] = '%d புகைப்படங்களைப் பதிவேற்றியவர் %s';
+$lang['%d photos uploaded into album "%s"'] = '%d புகைப்படங்கள் ஆல்பத்திற்குப் பதிவேற்றப்பட்டது "%s"';
+$lang['%d photos validated'] = '%d புகைப்படங்கள் மதிப்பிடப்பட்டது';
+$lang['%s (the group)'] = '%s (குழுமம்)';
+$lang['%s (the user)'] = '%s (பயனர்)';
+$lang['%u pending photos'] = '%u முடிவுபெறாத புகைப்படங்கள்';
+$lang['Add a permission'] = 'ஒரு அனுமதியைச் சேர்';
+$lang['Cancel'] = 'ரத்து';
+$lang['Delete permission'] = 'அனுமதி நீக்கு';
+$lang['Edit a permission'] = 'அனுமதி திருத்த';
+$lang['Hi administrators,'] = 'நிர்வாகிகளுக்கு வணக்கம்,';
+$lang['Level 16'] = 'முடிவுபெறாத';
+$lang['No album, this photo is orphan'] = 'ஆல்பம் இல்லை, இந்த புகைப்படம் தனியாக உள்ளது';
+$lang['Pending Photos'] = 'முடிவுபெறாத புகைப்படங்கள்';
+$lang['Permission added'] = 'அனுமதி சேர்க்கப்பட்டது';
+$lang['Permission removed'] = 'அனுமதி நீக்கப்பட்டது';
+$lang['Permission updated'] = 'அனுமதி புதுப்பிக்கப்பட்டது';
+$lang['Set Photo Properties'] = 'புகைப்பட பண்புகள் அமைக்க';
+$lang['The whole gallery'] = 'முழு காட்சியகத் தொகுப்பு';
+?>
