a3a38f53fcb9090d44b77fc88637e606d5427f12
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-25 17:49:51 +0000

    [community] Add gl_ES, thanks to : damufo
    git-svn-id: http://piwigo.org/svn/extensions/community@21029 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/gl_ES/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Upload Permissions'] = 'Enviar permisos';
+$lang['Upload your own photos'] = 'Enviar túas propias fotos';
+$lang['Validation page: %s'] = 'Validación da páxina: %s';
+$lang['Where?'] = 'Onde?';
+$lang['Which level of trust?'] = 'Que nivel de confianza?';
+$lang['Who?'] = 'Quen?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Túas fotos están a agardar ser validadas, os administradores foron notificados';
+$lang['Zoom'] = 'Zun (zoom)';
+$lang['a group'] = 'un grupo';
+$lang['a specific user'] = 'un usuario especificado';
+$lang['ability to create sub-albums'] = 'permitir crear subálbums ';
+$lang['any registered user'] = 'calquera usuario rexistrado';
+$lang['any visitor'] = 'calquera visitante';
+$lang['high trust'] = 'moita confianza';
+$lang['low trust'] = 'pouco confianza';
+$lang['sub-albums creation'] = 'creación de subálbums';
+$lang['uploaded photos are directly displayed in the gallery'] = 'as fotos enviadas son amosadas directamente na galería';
+$lang['uploaded photos must be validated by an administrator'] = 'as fotos enviadas deben ser validadas por un administrador';
+$lang['web size'] = 'tamaño da web';
+$lang['Add a permission'] = 'Engadir permiso';
+$lang['Cancel'] = 'Cancelar';
+$lang['Delete permission'] = 'Eliminar permiso';
+$lang['Edit a permission'] = 'Modificar permiso';
+$lang['Hi administrators,'] = 'Ola administradores:';
+$lang['Level 16'] = 'Pendente';
+$lang['No album, this photo is orphan'] = 'Sen álbum, esta foto está orfa';
+$lang['Pending Photos'] = 'Fotos pendentes';
+$lang['Permission added'] = 'Permisos engadidos';
+$lang['Permission removed'] = 'Permisos eliminados';
+$lang['Permission updated'] = 'Permisos actualizados';
+$lang['Set Photo Properties'] = 'Propiedades do conxunto de fotos';
+$lang['The whole gallery'] = 'A galería enteira';
+$lang['%d photos rejected'] = '%d fotos rexeitadas';
+$lang['%d photos uploaded by %s'] = '%d fotos enviadas por %s';
+$lang['%d photos uploaded into album "%s"'] = '%d fotos enviadas ó álbum "%s"';
+$lang['%d photos validated'] = '%d fotos validadas';
+$lang['%s (the group)'] = '%s (no grupo)';
+$lang['%s (the user)'] = '%s (o usuario)';
+$lang['%u pending photos'] = '%u fotos pendentes';
+?>
