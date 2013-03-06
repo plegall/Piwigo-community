ea4e8bfae821f2f0142645f5b86fa145b1826670
plegall <plg@piwigo.org>
2013-03-06 16:45:33 +0000

    bug fixed: table creation crashed with mysqli
    specific code for pgsql/sqlite removed
    
    replace $conf['local_data_dir'] by $conf['data_location'] (introduced in Piwigo 2.4)
    
    
    git-svn-id: http://piwigo.org/svn/extensions/community@21289 68402e56-0260-453c-a942-63ccdbb3a9ee

--- maintain.inc.php
+++ maintain.inc.php
@@ -9,9 +9,7 @@ function plugin_install()
 {
   global $conf, $prefixeTable;
 
-  if ('mysql' == $conf['dblayer'])
-  {
-    $query = '
+  $query = '
 CREATE TABLE '.$prefixeTable.'community_permissions (
   id int(11) NOT NULL AUTO_INCREMENT,
   type varchar(255) NOT NULL,
@@ -24,9 +22,9 @@ CREATE TABLE '.$prefixeTable.'community_permissions (
   PRIMARY KEY (id)
 ) ENGINE=MyISAM DEFAULT CHARACTER SET utf8
 ;';
-    pwg_query($query);
+  pwg_query($query);
 
-    $query = '
+  $query = '
 CREATE TABLE '.$prefixeTable.'community_pendings (
   image_id mediumint(8) unsigned NOT NULL,
   state varchar(255) NOT NULL,
@@ -34,62 +32,7 @@ CREATE TABLE '.$prefixeTable.'community_pendings (
   validated_by smallint(5) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARACTER SET utf8
 ;';
-    pwg_query($query);
-  }
-  elseif ('pgsql' == $conf['dblayer'])
-  {
-    $query = '
-CREATE TABLE "'.$prefixeTable.'community_permissions" (
-  "id" serial NOT NULL,
-  "type" VARCHAR(255) NOT NULL,
-  "group_id" INTEGER,
-  "user_id" INTEGER,
-  "category_id" INTEGER,
-  "recursive" BOOLEAN default true,
-  "create_subcategories" BOOLEAN default false,
-  "moderated" BOOLEAN default true,
-  PRIMARY KEY ("id")
-)
-;';
-    pwg_query($query);
-
-    $query = '
-CREATE TABLE "'.$prefixeTable.'community_pendings" (
-  image_id INTEGER NOT NULL,
-  state VARCHAR(255) NOT NULL,
-  added_on TIMESTAMP NOT NULL,
-  validated_by INTEGER
-)
-;';
-    pwg_query($query);
-  }
-  else
-  {
-    $query = '
-CREATE TABLE "'.$prefixeTable.'community_permissions" (
-  "id" INTEGER NOT NULL,
-  "type" VARCHAR(255) NOT NULL,
-  "group_id" INTEGER,
-  "user_id" INTEGER,
-  "category_id" INTEGER,
-  "recursive" BOOLEAN default true,
-  "create_subcategories" BOOLEAN default false,
-  "moderated" BOOLEAN default true,
-  PRIMARY KEY ("id")
-)
-;';
-    pwg_query($query);
-
-    $query = '
-CREATE TABLE "'.$prefixeTable.'community_pendings" (
-  image_id INTEGER NOT NULL,
-  state VARCHAR(255) NOT NULL,
-  added_on TIMESTAMP NOT NULL,
-  validated_by INTEGER
-)
-;';
-    pwg_query($query);
-  }
+  pwg_query($query);
 }
 
 function plugin_uninstall()
@@ -146,7 +89,7 @@ function community_get_data_from_core21()
 {
   global $conf, $prefixeTable;
   
-  $from_piwigo21_file = $conf['local_data_dir'].'/plugins/core_user_upload_to_community.php';
+  $from_piwigo21_file = $conf['data_location'].'/plugins/core_user_upload_to_community.php';
   if (is_file($from_piwigo21_file))
   {
     include($from_piwigo21_file);
