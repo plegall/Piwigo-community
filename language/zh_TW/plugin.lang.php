aed8a618c43ff629aba5ebff50eef1d6c85d361e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-15 05:51:46 +0000

    [community] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/community@21562 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['high trust'] = '高度信任';
+$lang['web size'] = '網站大小';
+$lang['uploaded photos must be validated by an administrator'] = '上傳的照片必須由管理員進行驗證';
+$lang['uploaded photos are directly displayed in the gallery'] = '上傳的照片直接顯示在畫廊';
+$lang['sub-albums creation'] = '子專輯建立';
+$lang['low trust'] = '低信任度';
+$lang['any visitor'] = '任何訪客';
+$lang['any registered user'] = '任何註冊用戶都';
+$lang['ability to create sub-albums'] = '可建立子專輯';
+$lang['a specific user'] = '一個特定的用戶';
+$lang['a group'] = '一組';
+$lang['Zoom'] = '放大';
+$lang['Your photos are waiting for validation, administrators have been notified'] = '您的照片會等待驗證，已經通知管理員';
+$lang['Who?'] = '是誰?';
+$lang['Which level of trust?'] = '哪個級別的信任?';
+$lang['Where?'] = '在哪裡?';
+$lang['Validation page: %s'] = '驗證頁面: %s';
+$lang['Upload your own photos'] = '上傳您自己的照片';
+$lang['Upload Permissions'] = '上傳權限';
+$lang['The whole gallery'] = '整個畫廊';
+$lang['Set Photo Properties'] = '設定照片屬性';
+$lang['Permission updated'] = '權限已更新';
+$lang['Permission removed'] = '權限已移除';
+$lang['Permission added'] = '權限已新增';
+$lang['No album, this photo is orphan'] = '沒有專輯，這張照片是孤兒';
+$lang['Pending Photos'] = '待處理照片';
+$lang['%u pending photos'] = '%u 個待處理照片';
+$lang['Level 16'] = '待處理';
+$lang['Hi administrators,'] = '您好系統管理員，';
+$lang['Edit a permission'] = '編輯權限';
+$lang['Delete permission'] = '刪除權限';
+$lang['Cancel'] = '取消';
+$lang['Add a permission'] = '增加一個權限';
+$lang['%s (the user)'] = '%s (用戶)';
+$lang['%s (the group)'] = '%s (組)';
+$lang['%d photos validated'] = '%d 張照片經過驗證';
+$lang['%d photos uploaded into album "%s"'] = '%d 張照片上傳到專輯 "%s"';
+$lang['%d photos uploaded by %s'] = '%d 張照片由 %s 上傳';
+$lang['%d photos rejected'] = '%d 照片被拒絕接受';
+?>
