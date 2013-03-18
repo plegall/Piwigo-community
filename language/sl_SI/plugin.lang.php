0518c0f8727435a6205da82a766167dddc66ac31
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-18 20:38:13 +0000

    [community] Add sl_SI, thanks to : MarkoAm
    git-svn-id: http://piwigo.org/svn/extensions/community@21681 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Upload Permissions'] = 'Naloži dovoljenja';
+$lang['Upload your own photos'] = 'Naloži svoje fotografije';
+$lang['Validation page: %s'] = 'Stran preverjanja: %s';
+$lang['Where?'] = 'Kje?';
+$lang['Which level of trust?'] = 'Katera stopnja zaupanja?';
+$lang['Who?'] = 'kdo?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Vaše fotografije čakajo preverjanje. Skrbniki so bili obveščeni.';
+$lang['Zoom'] = 'Povečaj';
+$lang['a group'] = 'skupina';
+$lang['a specific user'] = 'določen uporabnik';
+$lang['ability to create sub-albums'] = 'možnost ustvarjanja pod-albuma';
+$lang['any registered user'] = 'vsak registriran uporabnik';
+$lang['any visitor'] = 'vsak obiskovalec';
+$lang['high trust'] = 'visoko zaupanje';
+$lang['low trust'] = 'nizko zaupanje';
+$lang['sub-albums creation'] = 'kreiranje pod-albuma';
+$lang['uploaded photos are directly displayed in the gallery'] = 'naložene fotografije so prikazane neposredno v galeriji';
+$lang['uploaded photos must be validated by an administrator'] = 'naložene slike mora preveriti skrbnik';
+$lang['web size'] = 'spletna velikost';
+$lang['%d photos rejected'] = '%d slik zavrnjenih';
+$lang['%d photos uploaded by %s'] = '%d fotografije naložil %s';
+$lang['%d photos uploaded into album "%s"'] = '%d slik naloženih v album "%s"';
+$lang['%d photos validated'] = '%d fotografij preverjenih';
+$lang['%s (the group)'] = '%s (skupina)';
+$lang['%s (the user)'] = '%s (uporabnik)';
+$lang['%u pending photos'] = '%u fotografije na čakanju';
+$lang['Add a permission'] = 'dodaj dovoljenje';
+$lang['Cancel'] = 'Prekliči';
+$lang['Delete permission'] = 'Odstrani dovoljenje';
+$lang['Edit a permission'] = 'Uredi dovoljenje';
+$lang['Hi administrators,'] = 'Pozdravljeni administratorji,';
+$lang['Level 16'] = 'Na čakanju';
+$lang['No album, this photo is orphan'] = 'Ni albuma, ta slika je sirota';
+$lang['Pending Photos'] = 'Fotografije na čakanju';
+$lang['Permission added'] = 'Dovoljenje dodano';
+$lang['Permission removed'] = 'Dovoljenje odstranjeno';
+$lang['Permission updated'] = 'Dovoljenje posodobljeno';
+$lang['Set Photo Properties'] = 'Nastavi lastnosti fotografije';
+$lang['The whole gallery'] = 'Celotna galerija';
+?>
