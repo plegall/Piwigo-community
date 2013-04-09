fb82c58dfa7a58b135faca852de794e19ed0b3e2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-09 21:05:10 +0000

    [community] Add nn_NO, thanks to : kiho
    git-svn-id: http://piwigo.org/svn/extensions/community@22108 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nn_NO/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Upload Permissions'] = 'Last opp løyve';
+$lang['Upload your own photos'] = 'Last opp dine eigne bilete';
+$lang['Validation page: %s'] = 'Valideringsside: %s';
+$lang['Where?'] = 'Kvar?';
+$lang['Which level of trust?'] = 'Kva for klareringsnivå?';
+$lang['Who?'] = 'Kven?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Bileta dine ventar på validering, administrator har fått påminning';
+$lang['Zoom'] = 'Zoom';
+$lang['a group'] = 'ei gruppe';
+$lang['a specific user'] = 'ein spesifikk brukar';
+$lang['ability to create sub-albums'] = 'moglegheit til å lage underalbum';
+$lang['any registered user'] = 'alle registrera brukarar';
+$lang['any visitor'] = 'alle gjester';
+$lang['high trust'] = 'høgt klareringsnivå';
+$lang['low trust'] = 'lagt klareringsnivå';
+$lang['sub-albums creation'] = 'lage underalbum';
+$lang['uploaded photos are directly displayed in the gallery'] = 'bilete som blir lasta opp blir vist i galleriet med ei gong';
+$lang['uploaded photos must be validated by an administrator'] = 'bilete som blir lasta opp må validerast av ein administrator';
+$lang['web size'] = 'størrelse på nettsida';
+$lang['%d photos rejected'] = '%d bilete avvist';
+$lang['%d photos uploaded by %s'] = '%d bilete lasta opp av %s';
+$lang['%d photos uploaded into album "%s"'] = '%d bilete lasta opp til albumet %s';
+$lang['%d photos validated'] = '%d bilete validera';
+$lang['%s (the group)'] = '%s (gruppa)';
+$lang['%s (the user)'] = '%s (brukaren)';
+$lang['%u pending photos'] = '%u ventande bilete';
+$lang['Add a permission'] = 'Legg til løyve';
+$lang['Cancel'] = 'Avbryt';
+$lang['Delete permission'] = 'Fjern løyve';
+$lang['Edit a permission'] = 'Endre eit løyve';
+$lang['Hi administrators,'] = 'Hei administratorar,';
+$lang['Level 16'] = 'Ventande';
+$lang['No album, this photo is orphan'] = 'Ikkje noko album, dette biletet høyrer ikkje til noko album';
+$lang['Pending Photos'] = 'Ventande bilete';
+$lang['Permission added'] = 'Løyve er lagt til';
+$lang['Permission removed'] = 'Løyve er fjerna';
+$lang['Permission updated'] = 'Løyve er oppdatera';
+$lang['Set Photo Properties'] = 'Set bileteigenskapar';
+$lang['The whole gallery'] = 'Heile galleriet';
+?>
