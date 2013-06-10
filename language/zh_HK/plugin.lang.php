95d07711ce66d9814b99b784512e5a360ac7cfa5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-10 04:21:16 +0000

    [community] Add zh_HK, thanks to : hkdigit
    git-svn-id: http://piwigo.org/svn/extensions/community@23123 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_HK/plugin.lang.php
@@ -0,0 +1,81 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Who?'] = '誰？';
+$lang['Your photos are waiting for validation, administrators have been notified'] = '你的相片正進行驗證，管理員已經通知';
+$lang['Zoom'] = '放大';
+$lang['a group'] = '一組';
+$lang['a specific user'] = '特定使用者';
+$lang['ability to create sub-albums'] = '創建子相簿能力';
+$lang['any registered user'] = '任何註冊用戶';
+$lang['any visitor'] = '任何訪客';
+$lang['high trust'] = '高度信任';
+$lang['low trust'] = '低度信任';
+$lang['no limit'] = '沒有限制';
+$lang['sub-albums creation'] = '建立子相簿';
+$lang['up to %d photos (for each user)'] = '最多 %d 張相片 (每個使用者)';
+$lang['up to %dMB (for each user)'] = '最多 %dMB (每個使用者)';
+$lang['uploaded photos are directly displayed in the gallery'] = '上傳的相片直接顯示在圖片庫上';
+$lang['uploaded photos must be validated by an administrator'] = '上載相片必須經由管理員驗證';
+$lang['web size'] = '網絡大小';
+$lang['How much disk space?'] = '多少磁碟空間？';
+$lang['Level 16'] = '待';
+$lang['Maximum number of photos reached (%u)'] = '已達到最多相片數(%u)';
+$lang['No album, this photo is orphan'] = '沒有相簿，這張相片是孤兒';
+$lang['Pending Photos'] = '待處理相片';
+$lang['Permission added'] = '已加權限';
+$lang['Permission removed'] = '權限已刪除';
+$lang['Permission updated'] = '權限已更新';
+$lang['Photo %s rejected.'] = '相片 %s 拒絕了。';
+$lang['Piwigo automatically creates an album for each user, on first connection'] = 'Piwigo自動為每個用戶在第一次連接表時,建立一個相簿
+';
+$lang['Set Photo Properties'] = '設定相片屬性';
+$lang['The whole gallery'] = '整個圖片庫';
+$lang['Upload Permissions'] = '上載權限';
+$lang['Upload your own photos'] = '上載自己的相片';
+$lang['User album only'] = '用戶相簿';
+$lang['User albums'] = '用戶相簿';
+$lang['Validation page: %s'] = '驗證網頁: %s';
+$lang['Where should Piwigo create user albums?'] = '應該在哪裡創建Piwigo使用者相簿？';
+$lang['Where?'] = '在哪裡？';
+$lang['Which level of trust?'] = '哪個信任級別？';
+$lang['%d photos rejected'] = '%d張相片拒絕了';
+$lang['%d photos uploaded by %s'] = '%d張相片由%s上傳';
+$lang['%d photos uploaded into album "%s"'] = '%d張相上載到"%s"相簿';
+$lang['%d photos validated'] = '%d張相片經過驗證';
+$lang['%s (the group)'] = '%s (組)';
+$lang['%s (the user)'] = '%s (使用者)';
+$lang['%s out of %s'] = '%s來自%s';
+$lang['%u pending photos'] = '%u 張相片待處理';
+$lang['(in addition to user album)'] = '(除使用者相簿之外)';
+$lang['Add a permission'] = '新增權限';
+$lang['Available %s.'] = '可用的%s.';
+$lang['Available quota %s.'] = '可用配額%s.';
+$lang['Cancel'] = '取消';
+$lang['Delete permission'] = '刪除權限';
+$lang['Disk usage quota reached (%uMB)'] = '磁盤限額已達(%uMB)';
+$lang['Edit a permission'] = '修改權限';
+$lang['File %s too big (%uMB), quota of %uMB exceeded'] = '檔案 %s 太大 (%uMB), 配額 %uMB 已超過';
+$lang['Gallery root'] = '圖片庫主目錄';
+$lang['Hi administrators,'] = '管理員你好，';
+$lang['How many photos?'] = '幾多張相片？';
+?>
