d3fd4e3a6b3175dc0fa7a542f213f427a1b3c55c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-30 10:37:45 +0000

    [community] Add lt_LT, thanks to : vilisas
    git-svn-id: http://piwigo.org/svn/extensions/community@23703 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lt_LT/plugin.lang.php
@@ -0,0 +1,80 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['(in addition to user album)'] = '(kartu su vartotojo albumu)';
+$lang['a group'] = 'grupė';
+$lang['a specific user'] = 'konkretus vartotojas';
+$lang['ability to create sub-albums'] = 'galimybė kurti subalbumus';
+$lang['any registered user'] = 'bet kuris registruotas vartotojas ';
+$lang['any visitor'] = 'bet kuris lankytojas';
+$lang['high trust'] = 'aukštas pasitikėjimas';
+$lang['low trust'] = 'žemas pasitikėjimas';
+$lang['no limit'] = 'be apribojimų';
+$lang['sub-albums creation'] = 'subalbumų kūrimas';
+$lang['up to %d photos (for each user)'] = 'iki %d nuotraukų (kiekvienam vartotojui)';
+$lang['up to %dMB (for each user)'] = 'iki %dMB (kiekvienam vartotojui)';
+$lang['uploaded photos are directly displayed in the gallery'] = 'nuotraukos rodomos iš karto, po įkėlimo į galeriją';
+$lang['uploaded photos must be validated by an administrator'] = 'nuotraukos turi būti patvirtintos administratoriaus';
+$lang['web size'] = 'web dydis';
+$lang['User albums'] = 'Vartotojo albumai';
+$lang['Validation page: %s'] = 'Patvirtinimų puslapis: %s';
+$lang['Where should Piwigo create user albums?'] = 'Kur Piwigo turėtų kurti vartotojų albumus?';
+$lang['Where?'] = 'Kur?';
+$lang['Which level of trust?'] = 'Koks pasitikėjimo lygis?';
+$lang['Who?'] = 'Kas?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Jūsų nuotraukos laukia patvirtinimo, administratoriai informuoti';
+$lang['Zoom'] = 'Priartinti';
+$lang['%d photos validated'] = '%d nuotraukų patvirtinta';
+$lang['Gallery root'] = 'Galerijos šaknis';
+$lang['Hi administrators,'] = 'Sveiki administratoriai,';
+$lang['How many photos?'] = 'Kiek nuotraukų?';
+$lang['How much disk space?'] = 'Kiek disko vietos?';
+$lang['Level 16'] = 'Laukiama';
+$lang['Maximum number of photos reached (%u)'] = 'Pasiektas maksimalus nuotraukų skaičius (%u)';
+$lang['No album, this photo is orphan'] = 'Ši nuotrauka nepriklauso jokiam albumui';
+$lang['Pending Photos'] = 'Laukiančios nuotraukos';
+$lang['Permission added'] = 'Leidimas pridėtas';
+$lang['Permission removed'] = 'Leidimas panaikintas';
+$lang['Permission updated'] = 'Leidimas atnaujintas';
+$lang['Photo %s rejected.'] = 'Nuotrauka %s atmesta.';
+$lang['Piwigo automatically creates an album for each user, on first connection'] = 'Piwigo automatiškai sukuria albuma kiekvienam vartotojui, kai jis pirmą karta prisijungia.';
+$lang['Set Photo Properties'] = 'Nustatyti nuotraukų savybes';
+$lang['The whole gallery'] = 'Visa galerija';
+$lang['Upload Permissions'] = 'Įkrovos leidimai';
+$lang['Upload your own photos'] = 'Įkelti savo nuotraukas';
+$lang['User album only'] = 'Tik vartotojo albumas';
+$lang['%d photos rejected'] = '%d nuotraukos atmestos';
+$lang['%d photos uploaded by %s'] = '%d nuotraukų įkėlė %s';
+$lang['%d photos uploaded into album "%s"'] = '%d nuotraukų įkelta į albumą "%s"';
+$lang['%s (the group)'] = '%s (grupė)';
+$lang['%s (the user)'] = '%s (vartotojas)';
+$lang['%s out of %s'] = '%s iš %s';
+$lang['%u pending photos'] = '%u nuotraukų laukia';
+$lang['Add a permission'] = 'Pridėti leidimą';
+$lang['Available %s.'] = 'Prieinama %s.';
+$lang['Available quota %s.'] = 'Prieinama kvota %s.';
+$lang['Cancel'] = 'Atšaukti';
+$lang['Delete permission'] = 'Panaikinti leidimus';
+$lang['Disk usage quota reached (%uMB)'] = 'Viršyta disko kvota (%uMB)';
+$lang['Edit a permission'] = 'Redaguoti leidimus';
+$lang['File %s too big (%uMB), quota of %uMB exceeded'] = 'Failas %s per didelis (%uMB), kai kvota yra %uMB';
+?>
