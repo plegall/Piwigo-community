d7d8bd575193ab0d60633f4c9ffcfe34749d52de
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-03-26 20:10:30 +0000

    [community] Update sr_RS, thanks to : gpopac
    git-svn-id: http://piwigo.org/svn/extensions/community@27972 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sr_RS/plugin.lang.php
@@ -0,0 +1,81 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Gallery root'] = 'Почетна галерије';
+$lang['up to %d photos (for each user)'] = 'до %d фотографија (за сваког корисника)';
+$lang['up to %dMB (for each user)'] = 'до %dMB (за сваког корисника)';
+$lang['uploaded photos are directly displayed in the gallery'] = 'послате фотографије се одмах приказују у галерији';
+$lang['uploaded photos must be validated by an administrator'] = 'администратор мора фа провери послате фотографије';
+$lang['web size'] = 'величина за веб';
+$lang['Where should Piwigo create user albums?'] = 'Где да Piwigo направи корисничке албуме?';
+$lang['Which level of trust?'] = 'До ког нивоа веровати?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Ваше фотографије чекају на потврду администратора';
+$lang['a user can own only one album'] = 'Корисник може да има само по један албум';
+$lang['ability to create sub-albums'] = 'могућност образовања подалбума';
+$lang['high trust'] = 'високо поверење';
+$lang['low trust'] = 'ниско поверење';
+$lang['no limit'] = 'без ограничења';
+$lang['sub-albums creation'] = 'образовање подалбума';
+$lang['Validation page: %s'] = 'Страна за потврду фотографија: %s';
+$lang['Zoom'] = 'Увећање';
+$lang['a group'] = 'група';
+$lang['a specific user'] = 'одређени корисник';
+$lang['any registered user'] = 'било који регистровани корисник';
+$lang['any visitor'] = 'било који посетилац';
+$lang['Maximum number of photos reached (%u)'] = 'Највећи број фотографије је досегао (%u)';
+$lang['Permission removed'] = 'Дозвола је уклоњена';
+$lang['Permission updated'] = 'Дозвола је унапређена';
+$lang['Photo %s rejected.'] = 'Фотографије %s је одбијена.';
+$lang['Piwigo automatically creates an album for each user, on first connection'] = 'Piwigo аутоматски прави један албум за сваког корисника приликом прве посете';
+$lang['Set Photo Properties'] = 'Поставите особине фотографије';
+$lang['The whole gallery'] = 'Цела галерија';
+$lang['Upload Permissions'] = 'Пошаљи дозволе';
+$lang['Upload your own photos'] = 'Пошаљи сопствене фотографије';
+$lang['User album only'] = 'Само кориснички албум';
+$lang['User albums'] = 'Кориснички албум';
+$lang['Where?'] = 'Где?';
+$lang['Who?'] = 'Ко?';
+$lang['%d photos validated'] = 'Проверено је %d фотографије';
+$lang['File %s too big (%uMB), quota of %uMB exceeded'] = 'Датотека %s је превелика (%uMB), јер преко квоту од %uMB';
+$lang['(in addition to user album)'] = '(као додатак корисничком албуму)';
+$lang['Add a permission'] = 'Додај дозволу';
+$lang['Album of user'] = 'Кориснички албум';
+$lang['Available %s.'] = 'Доступно %s.';
+$lang['Available quota %s.'] = 'Доступна квота %s.';
+$lang['Cancel'] = 'Откажи';
+$lang['Delete permission'] = 'Обриши дозволу';
+$lang['Disk usage quota reached (%uMB)'] = 'Употреба диска и квота је достигла (%uMB)';
+$lang['Edit a permission'] = 'Уреди дозволу';
+$lang['Hi administrators,'] = 'Поздрав администратори,';
+$lang['How many photos?'] = 'Колико фотографија?';
+$lang['How much disk space?'] = 'Колико простора на диску?';
+$lang['Level 16'] = 'На чекању';
+$lang['No album, this photo is orphan'] = 'Нема албума, ова слика је сироче';
+$lang['Pending Photos'] = 'Фотографије на чекању';
+$lang['Permission added'] = 'Дозвола је додата';
+$lang['%d photos rejected'] = 'Одбијено је %d слика';
+$lang['%d photos uploaded by %s'] = '%s је додао %d слика';
+$lang['%d photos uploaded into album "%s"'] = 'Додато је %d слика у албум „%s“';
+$lang['%s (the group)'] = '%s (група)';
+$lang['%s (the user)'] = '%s (корисник)';
+$lang['%s out of %s'] = '%s од %s';
+$lang['%u pending photos'] = '%u слика на чекању';
