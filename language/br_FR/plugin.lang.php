8dfae6bd5a28e5be1346a7aa41888d38518831b6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-01-02 16:01:14 +0000

    Update br_FR, thanks to yves.kerleguer
    git-svn-id: http://piwigo.org/svn/extensions/community@30840 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/br_FR/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['How many photos?'] = 'Pet luc\'hskeudenn ?';
+$lang['How much disk space?'] = 'Pegement egor war ar bladenn ?';
+$lang['Level 16'] = 'O c\'hortoz';
+$lang['Gallery root'] = 'Penngavlec\'h ar palier';
+$lang['Hi administrators,'] = 'Demat merourien,';
+$lang['File %s too big (%uMB), quota of %uMB exceeded'] = 'Pounneroc\'h eo restr %s (%uMo) eget kota %uMo';
+$lang['Available %s.'] = 'Hegerz %s.';
+$lang['Available quota %s.'] = 'Kota hegerz %s.';
+$lang['Cancel'] = 'Nullañ';
+$lang['Delete permission'] = 'Dilemel an aotre';
+$lang['Disk usage quota reached (%uMB)'] = 'Kota implij ar bladenn a zo bet tizhet (%uMo)';
+$lang['Edit a permission'] = 'Aozañ un aotre';
+$lang['%d photos rejected'] = '%d luc\'hskeudenn nac\'het';
+$lang['%d photos uploaded by %s'] = '%d luc\'hskeudenn ouzhpennet gant %s';
+$lang['%d photos uploaded into album "%s"'] = '%d luc\'hskeudenn ouzhpennet d\'ar rummad "%s"';
+$lang['%d photos validated'] = '%d luc\'hskeudenn gwiriekaet';
+$lang['%s (the group)'] = '%s (ar strollad)';
+$lang['%s (the user)'] = '%s (an implijer)';
+$lang['%s out of %s'] = '%s war %s';
+$lang['%u pending photos'] = '%u luc\'hseudenn o c\'hortoz';
+$lang['(in addition to user album)'] = '(ouzhpenn rummad an implijer)';
+$lang['Add a permission'] = 'Ouzhpennañ un aotre';
+$lang['Album of user'] = 'Rummad an implijer';
