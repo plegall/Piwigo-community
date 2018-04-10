0196057c98b73d9b1fa04b025e65c890370e99e2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-10 06:03:00 +0200

    Add ja_JP, thanks to Trumpet

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,80 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%s out of %s'] = '%s 中の %s';
+$lang['%u pending photos'] = '%u個の待機中写真';
+$lang['Gallery root'] = 'ギャラリーのルート';
+$lang['no limit'] = '制限なし';
+$lang['sub-albums creation'] = 'サブアルバムの生成';
+$lang['web size'] = 'ウェブサイズ';
+$lang['Available quota %s.'] = '可能なディスク容量は%sです。';
+$lang['Hi administrators,'] = '管理者さんこんにちは、';
+$lang['How many photos?'] = '何枚の写真ですか？';
+$lang['How much disk space?'] = 'どのくらいのディスク容量ですか？';
+$lang['Set Photo Properties'] = '写真のプロパティを設定する';
+$lang['The whole gallery'] = 'すべてのギャラリー';
+$lang['Validation page: %s'] = '検証ページ：%s';
+$lang['Which level of trust?'] = 'どのレベルの信頼性ですか？';
+$lang['a group'] = 'グループ';
+$lang['a specific user'] = '特定のユーザ';
+$lang['a user can own only one album'] = 'ユーザはただ1つのアルバムを所有できます';
+$lang['ability to create sub-albums'] = 'サブアルバムを生成する能力';
+$lang['any registered user'] = '登録済みユーザ';
+$lang['any visitor'] = '訪問者';
+$lang['high trust'] = '高信頼';
+$lang['low trust'] = '低信頼';
+$lang['up to %d photos (for each user)'] = '%d個までの写真（ユーザ毎に）';
+$lang['up to %dMB (for each user)'] = '%dMBまで（ユーザ毎に）';
+$lang['uploaded photos are directly displayed in the gallery'] = 'アップロードされた写真はギャラリーに直接表示されます';
+$lang['uploaded photos must be validated by an administrator'] = 'アップロードされた写真は管理者によって検証されなければなりません';
+$lang['Upload your own photos'] = '貴方自身の写真をアップロードする';
+$lang['User album only'] = 'ユーザアルバムだけ';
+$lang['User albums'] = 'ユーザアルバム';
+$lang['Where should Piwigo create user albums?'] = 'Piwigoはどこにユーザのアルバムを作成しますか？';
+$lang['Where?'] = 'どこですか？';
+$lang['Who?'] = '誰ですか？';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'あなたの写真は検証待ちです、管理者に通知しています';
+$lang['Zoom'] = 'ズーム';
+$lang['Level 16'] = '保留';
+$lang['Maximum number of photos reached (%u)'] = '写真数の上限に到達しました（%u）';
+$lang['No album, this photo is orphan'] = 'アルバムがありません、この写真は迷子です';
+$lang['Pending Photos'] = '保留中の写真';
+$lang['Permission added'] = '権限が追加されました';
+$lang['Permission removed'] = '権限が削除されました';
+$lang['Permission updated'] = '権限が更新されました';
+$lang['Photo %s rejected.'] = '写真"%s"は拒否されました。';
+$lang['Piwigo automatically creates an album for each user, on first connection'] = 'Piwigoは各々のユーザが最初に接続した際にアルバムを自動的に作成します';
+$lang['Upload Permissions'] = '権限をアップロードする';
+$lang['%d photos rejected'] = '%d個の写真が拒否されました';
+$lang['%d photos uploaded by %s'] = '%d個の写真が%sによってアップロードされました';
+$lang['%d photos uploaded into album "%s"'] = '%d個の写真がアルバム"%s"にアップロードされました';
+$lang['%d photos validated'] = '%d個の写真が検証されました';
+$lang['%s (the group)'] = '%s （グループ）';
+$lang['%s (the user)'] = '%s （ユーザ）';
+$lang['(in addition to user album)'] = '（ユーザアルバムに加えて）';
+$lang['Add a permission'] = '権限を追加';
+$lang['Album of user'] = 'ユーザのアルバム';
+$lang['Cancel'] = 'キャンセル';
+$lang['Delete permission'] = '権限を削除';
+$lang['Disk usage quota reached (%uMB)'] = 'ディスク使用量の制限に到達した(%uMB)';
+$lang['Edit a permission'] = '権限を編集';
+$lang['File %s too big (%uMB), quota of %uMB exceeded'] = 'ファイル"%s"は大きすぎて（%uMB）、ディスク使用量の制限%uMBを超過します';
