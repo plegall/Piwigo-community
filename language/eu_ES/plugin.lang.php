92e75135ebee303d2187dc737a87e2cf96a56546
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-02-17 12:30:54 +0100

    Add eu_ES, thanks to bixerdo

---
+++ language/eu_ES/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Where?'] = 'Non?';
+$lang['Which level of trust?'] = 'Zein konfiantza-maila?';
+$lang['Who?'] = 'Nork?';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Zure argazkiak balidazioaren zain dadue, administratzaileei jakinarazi egin zaie';
+$lang['Zoom'] = 'Zoom';
+$lang['a group'] = 'talde bat';
+$lang['a specific user'] = 'erabiltzaile jakin bat';
+$lang['a user can own only one album'] = 'album bakarra baino ez erabiltzaileko';
+$lang['ability to create sub-albums'] = 'azpi-albumak sortzeko gaitasuna';
+$lang['any registered user'] = 'erregistraturiko edozein erabiltzaile';
+$lang['any visitor'] = 'edozein bisitari';
+$lang['high trust'] = 'konfiantza gorena';
+$lang['low trust'] = 'konfiantza baxua';
+$lang['no limit'] = 'limiterik gabe';
+$lang['sub-albums creation'] = 'azpi-albumak sortzea';
+$lang['up to %d photos (for each user)'] = '%d(e)tik gorako argazkiak (erabiltzaileko)';
+$lang['up to %dMB (for each user)'] = '%dMB(e)tik gora (erabiltzaileko)';
+$lang['uploaded photos are directly displayed in the gallery'] = 'kargatutako argazkiak zuzenean bistaratzen dira galerian';
+$lang['uploaded photos must be validated by an administrator'] = 'kargatutako argazkiak administratzaile batek balidatu behar ditu';
+$lang['web size'] = 'web-tamaina';
+$lang['%d photos uploaded into album "%s"'] = '%d argazki kargatu dira "%s" albumera';
+$lang['%d photos uploaded by %s'] = '%d argazki karagatu ditu %s(e)k ';
+$lang['How many photos?'] = 'Zenbat argazki?';
+$lang['How much disk space?'] = 'Zenbat leku diskoan?';
+$lang['Level 16'] = 'Zain';
+$lang['Maximum number of photos reached (%u)'] = 'Gehienezko argazki kopurura iritsi zara (%u)';
+$lang['No album, this photo is orphan'] = 'Ez du albumik, argazkia umezurtz dago';
+$lang['Pending Photos'] = 'Argazkiak zain';
+$lang['Permission added'] = 'Gehitu egin da baimena';
+$lang['Permission removed'] = 'Kendu egin da baimena';
+$lang['Permission updated'] = 'Eguneratu egin da baimena';
+$lang['Photo %s rejected.'] = 'Baztertu egin da %s argazkia.';
+$lang['Piwigo automatically creates an album for each user, on first connection'] = 'Piwigok automatikoki sortzen du albuma lehen aldiz konektatzen den erabiltzaile bakoitzeko';
+$lang['Set Photo Properties'] = 'Ezarri argazkiaren propietateak';
+$lang['The whole gallery'] = 'Galeria osoa';
+$lang['Upload Permissions'] = 'Kargatu baimenak';
+$lang['Upload your own photos'] = 'Kargatu zure argazkiak';
+$lang['User album only'] = 'Erabiltzailearen albumean soilik';
+$lang['User albums'] = 'Erabiltzailearen albumak';
+$lang['Validation page: %s'] = 'Balidazio-orria: %s';
+$lang['Where should Piwigo create user albums?'] = 'Non sortu behar ditu Piwigok erabiltzaileen albumak?';
+$lang['%d photos validated'] = '%d argazki balidatu dira';
+$lang['%d photos rejected'] = '%d argazki baztertuta';
+$lang['%s (the group)'] = '%s (taldea)';
+$lang['%s (the user)'] = '%s (erabiltzailea)';
+$lang['%s out of %s'] = '%s(e)tik %s';
+$lang['%u pending photos'] = '%u argazki zain';
+$lang['(in addition to user album)'] = '(erabiltzailearen albumera gehituz)';
+$lang['Add a permission'] = 'Gehitu baimena';
+$lang['Album of user'] = 'Erabiltzailearen albuma';
+$lang['Available %s.'] = '%s erabilgarri.';
+$lang['Available quota %s.'] = 'Kuota erabilgarria %s.';
+$lang['Cancel'] = 'Utzi';
+$lang['Delete permission'] = 'Ezabatu baimena';
+$lang['Disk usage quota reached (%uMB)'] = 'Diskoaren erabilera-kuotara iritsi zara (%uMB)';
+$lang['Edit a permission'] = 'Editatu baimena';
+$lang['File %s too big (%uMB), quota of %uMB exceeded'] = '%s fitxategia handiegia da (%uMB), %uMB (r)en kuota gainditu egin da';
+$lang['Gallery root'] = 'Galeri-erroa';
+$lang['Hi administrators,'] = 'Kaixo administratzaile,';
