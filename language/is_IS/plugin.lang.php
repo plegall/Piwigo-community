347e5e553cc68eab96af6a565f2a959babd22c0e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-05 07:10:17 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,66 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['web size'] = 'vefstærð';
+$lang['uploaded photos must be validated by an administrator'] = 'upphlaðnar myndir verða að vera staðfestar af stjórnanda';
+$lang['uploaded photos are directly displayed in the gallery'] = 'myndir sem hlaðnar hafa verið upp eru sýndar beint í galleríinu';
+$lang['up to %dMB (for each user)'] = 'allt að %dMB (fyrir hvern notanda)';
+$lang['up to %d photos (for each user)'] = 'allt að %d myndir (fyrir hvern notanda)';
+$lang['sub-albums creation'] = 'stofnun undiralbúma';
+$lang['no limit'] = 'engin takmörk';
+$lang['low trust'] = 'lítið traust';
+$lang['high trust'] = 'mikið traust';
+$lang['any visitor'] = 'hvaða gestur sem er';
+$lang['any registered user'] = 'hvaða skráða notanda sem er';
+$lang['ability to create sub-albums'] = 'geta til að búa til undiralbúm';
+$lang['a user can own only one album'] = 'notandi getur aðeins átt eitt albúm';
+$lang['a specific user'] = 'ákveðinn notandi';
+$lang['a group'] = 'hópur';
+$lang['Zoom'] = 'Þysja';
+$lang['Your photos are waiting for validation, administrators have been notified'] = 'Myndirnar þínar bíða eftir staðfestingu, stjórnendur hafa fengið tilkynningu';
+$lang['Who?'] = 'Hver?';
+$lang['Which level of trust?'] = 'Hvaða stigi trausts?';
+$lang['Where?'] = 'Hvar?';
+$lang['Where should Piwigo create user albums?'] = 'Hvar ætti Piwigo að stofna notandaalbúm?';
+$lang['Validation page: %s'] = 'Staðfestingarsíða: %s';
+$lang['User albums'] = 'Notandaalbúm';
+$lang['User album only'] = 'Aðeins notandaalbúm';
+$lang['Upload your own photos'] = 'Hladdu upp þínum eigin myndum';
+$lang['Upload Permissions'] = 'Upphleðsluheimildir';
+$lang['The whole gallery'] = 'Allt galleríið';
+$lang['Set Photo Properties'] = 'Stilltu myndeiginleika';
+$lang['Piwigo automatically creates an album for each user, on first connection'] = 'Piwigo býr sjálfkrafa til albúm fyrir hvern notanda, við fyrstu tengingu';
+$lang['Photo %s rejected.'] = 'Mynd %s hafnað.';
+$lang['Permission updated'] = 'Heimildir uppfærðar';
+$lang['Permission removed'] = 'Heimildir fjarlægðar';
+$lang['Permission added'] = 'Heimildum bætt við';
+$lang['Pending Photos'] = 'Myndir í bið';
+$lang['No album, this photo is orphan'] = 'Ekkert albúm, þessi mynd er munaðarlaus';
+$lang['Maximum number of photos reached (%u)'] = 'Hámarksfjölda mynda náð (%u)';
+$lang['Level 16'] = 'Í bið';
+$lang['How much disk space?'] = 'Hversu mikið diskpláss?';
+$lang['How many photos?'] = 'Hversu margar myndir?';
+$lang['Hi administrators,'] = 'Hæ stjórnendur,';
+$lang['Gallery root'] = 'Gallerí rót';
+$lang['File %s too big (%uMB), quota of %uMB exceeded'] = 'Skrá %s of stór (%uMB), farið yfir %uMB notkunarkvóta ';
+$lang['Edit a permission'] = 'Breyta heimildum';
+$lang['Disk usage quota reached (%uMB)'] = 'Notkunarkvóta disks náð (%uMB)';
+$lang['Add a permission'] = 'Bæta við heimildum';
+$lang['Delete permission'] = 'Eyða heimildum';
+$lang['Cancel'] = 'Hætta við';
+$lang['Available quota %s.'] = 'Laus kvóti %s.';
+$lang['Available %s.'] = 'Laust %s.';
+$lang['Album of user'] = 'Albúm notanda';
+$lang['(in addition to user album)'] = '(auk notendaalbúms)';
+$lang['%u pending photos'] = '%u myndir í bið';
+$lang['%s out of %s'] = '%s af %s';
+$lang['%s (the user)'] = '%s (notandinn)';
+$lang['%s (the group)'] = '%s (hópurinn)';
+$lang['%d photos validated'] = '%d myndir staðfestar';
+$lang['%d photos uploaded into album "%s"'] = '%d myndum hlaðið inn í albúm "%s"';
+$lang['%d photos uploaded by %s'] = '%d myndum hlaðið upp af %s';
+$lang['%d photos rejected'] = '%d myndum hafnað';
