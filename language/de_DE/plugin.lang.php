b7b32847b3d974c7e2c2cbb391dbbc3b8bb4f47a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-12-01 13:47:47 +0100

    Update de_DE, thanks to slim

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -93,3 +93,4 @@ $lang['Select at least one photo'] = 'Mindestens ein Foto muss ausgewählt sein'
 $lang['No photo can be deleted'] = 'Es kann kein Foto gelöscht werden';
 $lang['You need to confirm deletion'] = 'Der Löschvorgang muss bestätigt werden';
 $lang['No photo selected, no action possible.'] = 'Kein Foto ausgewählt, keine Aktion möglich.';
+$lang['Drag files here.'] = 'Ziehen Sie die Dateien hierher.';
